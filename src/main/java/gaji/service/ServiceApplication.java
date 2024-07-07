7bc105b6c79df45412f17170a5b7f87e2ee7f6f6
shimfff <shimseawon1510@gmail.com>
2024-07-07 23:14:22 +0900

    :sparkles: Feat: application 환경 분리

--- src/main/java/gaji/service/ServiceApplication.java
+++ src/main/java/gaji/service/ServiceApplication.java
@@ -3,10 +3,13 @@ package gaji.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class ServiceApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 		SpringApplication.run(ServiceApplication.class, args);
 	}
 
