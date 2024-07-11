e14549fa22df494aba56369242dfb83dbfce9223
shimfff <shimseawon1510@gmail.com>
2024-07-11 16:39:35 +0900

    :sparkles: Feat: RestApiException 추가

---
+++ src/main/java/gaji/service/global/common/exception/RestApiException.java
@@ -0,0 +1,13 @@
+package gaji.service.global.common.exception;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class RestApiException extends RuntimeException {
+
+    private final ErrorCodeInterface errorCode;
+
+    public ErrorCode getErrorCode() {
+        return this.errorCode.getErrorCode();
+    }
+}
