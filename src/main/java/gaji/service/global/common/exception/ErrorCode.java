718c69706c9af3d813043fcda1ef1103da3c7f7b
shimfff <shimseawon1510@gmail.com>
2024-07-11 16:37:06 +0900

    :sparkles: Feat: ErrorCode 추가

---
+++ src/main/java/gaji/service/global/common/exception/ErrorCode.java
@@ -0,0 +1,13 @@
+package gaji.service.global.common.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Builder
+public class ErrorCode {
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+}
