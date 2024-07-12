508b7021d4f415390c10fe920e83a54f648edfb3
shimfff <shimseawon1510@gmail.com>
2024-07-12 19:37:39 +0900

    :sparkles: Feat: /test/success 응답 삭제

--- src/main/java/gaji/service/domain/test/TestController.java
+++ src/main/java/gaji/service/domain/test/TestController.java
@@ -13,11 +13,4 @@ public class TestController {
         public String test() {
             return "test ok";
         }
-
-    //@Operation(summary = "성공적인 응답 반환 API", description = "테스트 문자열을 반환하는 API입니다.")
-    //@ApiResponse(responseCode = "200", description = "테스트 문자열을 성공적으로 반환")
-    @GetMapping("/success")
-    public BaseResponse<String> successResponseAPI() {
-        return BaseResponse.onSuccess("This is Test!");
-    }
 }
