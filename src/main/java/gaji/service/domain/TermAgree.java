09b565e5bf738b2620dfbc20f696827bb758a768
mmingoo <aldrn0323@naver.com>
2024-07-12 23:39:27 +0900

    ✨   Term, TermAgree 생성

---
+++ src/main/java/gaji/service/domain/TermAgree.java
@@ -0,0 +1,30 @@
+package gaji.service.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class TermAgree {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "term_id")
+    private Term term;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private LocalDate agreeDate;
+
+
+}
