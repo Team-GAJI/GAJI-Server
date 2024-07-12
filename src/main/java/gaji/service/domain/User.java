6639e4d3df776180b81006f6c908bd36b3a18a23
mmingoo <aldrn0323@naver.com>
2024-07-12 23:33:24 +0900

    ✨  user 생성

---
+++ src/main/java/gaji/service/domain/User.java
@@ -0,0 +1,50 @@
+package gaji.service.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
+
+    @Column(nullable = false)
+    private LocalDate birthday;
+
+    private LocalDateTime inactiveTime;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<SearchKeyword> searchKeywordList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "event", cascade = CascadeType.ALL)
+    private List<Event> eventList = new ArrayList<>();
+
+
+
+
+}
