aadc6b0d16d275d80835d05d4c49bdf31571334f
mmingoo <aldrn0323@naver.com>
2024-07-12 21:15:02 +0900

    🔥 API 응답통일 코드 삭제

--- src/main/java/gaji/service/domain/test/TempRequest.java
+++
@@ -1,4 +0,0 @@
-package gaji.service.domain.test;
-
-public class TempRequest {
-}
--- src/main/java/gaji/service/domain/test/TestController.java
+++ src/main/java/gaji/service/domain/test/TestController.java
@@ -1,6 +1,5 @@
 package gaji.service.domain.test;
 
-import gaji.service.apiPayload.ApiResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,12 +13,5 @@ public class TestController {
         public String test() {
             return "test ok";
         }
-
-        // 통일된 응답 테스트 , 성공한 경우
-        @GetMapping("/api/success")
-        public ApiResponse<TempResponse.TempTestDTO> successAPI(){
-            return ApiResponse.onSuccess(TempConverter.toSuccessTestDTO());
-        }
-
 
     }
