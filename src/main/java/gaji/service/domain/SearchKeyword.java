d2594674964e6148fe2bdf388b5687cd55707c70
mmingoo <aldrn0323@naver.com>
2024-07-12 23:37:43 +0900

    ✨   SearchKeyword, Event 생성

---
+++ src/main/java/gaji/service/domain/SearchKeyword.java
@@ -0,0 +1,23 @@
+package gaji.service.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SearchKeyword {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+}
