e57ba6395ce00b416c907de4ecccea75ddabf2da
mmingoo <aldrn0323@naver.com>
2024-07-12 23:39:27 +0900

    ✨   Term, TermAgree 생성

---
+++ src/main/java/gaji/service/domain/Term.java
@@ -0,0 +1,26 @@
+package gaji.service.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Term {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Boolean isRequired;
+
+    @OneToMany(mappedBy = "termAgree", cascade = CascadeType.ALL)
+    private List<TermAgree> termAgreeList = new ArrayList<>();
+}
