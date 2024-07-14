7372cb863b25bfaafc45f8259acf637ba87c3c1d
mmingoo <aldrn0323@naver.com>
2024-07-14 17:35:13 +0900

    ✨  Feat: Repot 생성

---
+++ src/main/java/gaji/service/domain/RecruitPost.java
@@ -0,0 +1,4 @@
+package gaji.service.domain;
+
+public class RecruitPost {
+}
---
+++ src/main/java/gaji/service/domain/Report.java
@@ -0,0 +1,24 @@
+package gaji.service.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Report {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "recruitPost")
+    private RecruitPost recruitPost;
+}
