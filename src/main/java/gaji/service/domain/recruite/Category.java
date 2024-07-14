2dbaaee09781bdae3bc82d0246ca2ea54fad4cc2
mmingoo <aldrn0323@naver.com>
2024-07-14 18:22:37 +0900

    ✨  Feat: Category 생성

---
+++ src/main/java/gaji/service/domain/recruite/Category.java
@@ -0,0 +1,19 @@
+package gaji.service.domain.recruite;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30)
+    private String name;
+}
