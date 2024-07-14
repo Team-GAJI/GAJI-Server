69ca65e6337f25c7e4d15e96b0b2f87b39a596d1
mmingoo <aldrn0323@naver.com>
2024-07-14 11:58:23 +0900

    ✨  roomNotice 생성

---
+++ src/main/java/gaji/service/domain/room/roomNotice.java
@@ -0,0 +1,22 @@
+package gaji.service.domain.room;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class roomNotice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    private String body;
+}
