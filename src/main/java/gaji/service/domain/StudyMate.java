c0a00429a8dc66e179c1c21f110c97e239f1f247
mmingoo <aldrn0323@naver.com>
2024-07-14 14:03:03 +0900

    ✨  Feat: RoomComment 생성

--- src/main/java/gaji/service/domain/StudyMate.java
+++ src/main/java/gaji/service/domain/StudyMate.java
@@ -1,10 +1,14 @@
 package gaji.service.domain;
 
 
+import gaji.service.domain.roomPost.RoomComment;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Builder
 @Entity
@@ -19,4 +23,9 @@ public class StudyMate {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     User user;
+
+    @OneToMany(mappedBy = "studyMate", cascade = CascadeType.ALL)
+    private List<RoomComment> roomCommentList = new ArrayList<>();
+
+
 }
---
+++ src/main/java/gaji/service/domain/roomPost/RoomComment.java
@@ -0,0 +1,43 @@
+package gaji.service.domain.roomPost;
+
+import gaji.service.domain.StudyMate;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoomComment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "studyMate_id")
+    private StudyMate studyMate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "roomPost_id")
+    private RoomPost roomPost;
+
+    // 부모 댓글, 자기 참조 방식
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private RoomComment parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<RoomComment> replies = new ArrayList<>();
+
+    private String CommentBody;
+    private Integer order;
+    private Integer depth;
+    private Boolean isDeleted;
+
+}
