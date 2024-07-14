be5a02c4f568e4201771949c86a0cbb036f69fa2
mmingoo <aldrn0323@naver.com>
2024-07-14 12:15:31 +0900

    ✨  RoomBoard, RoomPost 생성

---
+++ src/main/java/gaji/service/domain/roomPost/RoomBoard.java
@@ -0,0 +1,27 @@
+package gaji.service.domain.roomPost;
+
+import gaji.service.domain.room.Room;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoomBoard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
+    private List<RoomPost> roomPostList;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+}
