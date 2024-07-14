e0c9e81a07308c0c34923081c8a3bf98930aa9dd
mmingoo <aldrn0323@naver.com>
2024-07-14 20:26:01 +0900

    ✨  Feat: token 생성

---
+++ src/main/java/gaji/service/domain/token.java
@@ -0,0 +1,20 @@
+package gaji.service.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class token {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
