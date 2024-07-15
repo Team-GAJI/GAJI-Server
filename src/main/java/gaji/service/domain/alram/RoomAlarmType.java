118ce1fcd22cfe7daa7384a070847f41f7ed92d7
mmingoo <aldrn0323@naver.com>
2024-07-15 11:22:34 +0900

    ✨  Feat: RoomAlraTypem 생성

--- src/main/java/gaji/service/domain/alram/RoomAlarmType.java
+++ src/main/java/gaji/service/domain/alram/RoomAlarmType.java
@@ -1,4 +1,21 @@
 package gaji.service.domain.alram;
 
+import gaji.service.domain.enums.Type;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RoomAlarmType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long alarm;
+
+    @Enumerated(EnumType.STRING)
+    private Type type;
+
 }
