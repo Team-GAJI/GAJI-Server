fc8562dff987067fdfb23e8ed124493abba654f7
mmingoo <aldrn0323@naver.com>
2024-07-15 10:53:09 +0900

    ✨  Feat: Alram 생성

---
+++ src/main/java/gaji/service/domain/alram/Alarm.java
@@ -0,0 +1,22 @@
+package gaji.service.domain.alram;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import gaji.service.domain.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Alarm {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
