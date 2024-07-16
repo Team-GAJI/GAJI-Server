a3eee18a45dad00ea64856287ec531241a005401
spenshark <xzx01299@naver.com>
2024-07-16 16:30:28 +0900

    ♻️ Refactor: UUID 유틸리티 클래스 수정

--- src/main/java/gaji/service/util/uuid/Uuid.java
+++ src/main/java/gaji/service/util/uuid/Uuid.java
@@ -4,9 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Builder
 @Getter
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Uuid /* extends BaseEntity -> 추후 추가 시 주석 제거 */  {
 
@@ -16,4 +14,9 @@ public class Uuid /* extends BaseEntity -> 추후 추가 시 주석 제거 */  {
 
     @Column(unique = true)
     private String uuid;
+
+    @Builder
+    public Uuid(String uuid) {
+        this.uuid = uuid;
+    }
 }
