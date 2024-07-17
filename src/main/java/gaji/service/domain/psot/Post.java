b014de9d3fab5469a1bd2290fb4070c1aaf92713
mmingoo <aldrn0323@naver.com>
2024-07-17 21:40:06 +0900

    ✨  Feat: Post, PostType 생성

--- src/main/java/gaji/service/domain/psot/Post.java
+++ src/main/java/gaji/service/domain/psot/Post.java
@@ -1,4 +1,34 @@
 package gaji.service.domain.psot;
 
+import gaji.service.domain.User;
+import gaji.service.domain.enums.Status;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "type")
+    private PostType postType;
+
+    private String title;
+    private String body;
+    private int views;
+    private int likes;
+    private int bookmarks;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
 }
