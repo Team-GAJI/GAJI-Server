dfd50052e3d5ede01bbf434ede562e3d3a8358bf
shimfff <shimseawon1510@gmail.com>
2024-07-25 15:04:14 +0900

    :Sparkles: Feat: FIleCategory Enum으로 관리

--- src/main/java/gaji/service/domain/file/service/FileService.java
+++ src/main/java/gaji/service/domain/file/service/FileService.java
@@ -1,11 +1,12 @@
 package gaji.service.domain.file.service;
 
 import gaji.service.domain.file.dto.response.FileCreateResponse;
+import gaji.service.global.common.enums.FIleCategory;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
 
-    FileCreateResponse createFile(String domain, final MultipartFile file);
+    FileCreateResponse createFile(FIleCategory fIleCategory , final MultipartFile file);
 
     void deleteFile(String fileUrl);
 }
