30abfa64deefecda9912bed50e53e904d5710a6e
shimfff <shimseawon1510@gmail.com>
2024-07-25 15:05:03 +0900

    :sparkles: Feat: FIleCategory를 FileCategory로 오타 수정

--- src/main/java/gaji/service/domain/file/service/FileService.java
+++ src/main/java/gaji/service/domain/file/service/FileService.java
@@ -1,12 +1,12 @@
 package gaji.service.domain.file.service;
 
 import gaji.service.domain.file.dto.response.FileCreateResponse;
-import gaji.service.global.common.enums.FIleCategory;
+import gaji.service.global.common.enums.FileCategory;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
 
-    FileCreateResponse createFile(FIleCategory fIleCategory , final MultipartFile file);
+    FileCreateResponse createFile(FileCategory fIleCategory , final MultipartFile file);
 
     void deleteFile(String fileUrl);
 }
