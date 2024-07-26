965fb99aae8f2610a3c8931385b411019d02d71c
karryred <ujin037@gmail.com>
2024-07-26 17:38:23 +0900

    :sparkles: Feat: UserRepository 생성

--- src/main/java/gaji/service/domain/message/Message.java
+++ src/main/java/gaji/service/domain/message/Message.java
@@ -4,8 +4,10 @@ import gaji.service.domain.message.enums.MessageTypeEnum;
 import gaji.service.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
 
 import java.time.LocalDateTime;
 
@@ -36,6 +38,13 @@ public class Message {
     private LocalDateTime readDate;
     private LocalDateTime sendDate;
 
+    @Builder
+    public Message(User mine, User other, MessageTypeEnum type, LocalDateTime sendDate) {
+        this.myId = mine;
+        this.otherId = other;
+        this.type = type;
+        this.sendDate = sendDate;
+    }
 
 
 }
--- src/main/java/gaji/service/domain/message/service/MessageCommandServiceImpl.java
+++ src/main/java/gaji/service/domain/message/service/MessageCommandServiceImpl.java
@@ -3,14 +3,21 @@ package gaji.service.domain.message.service;
 import gaji.service.domain.message.Message;
 import gaji.service.domain.message.converter.MessageConverter;
 import gaji.service.domain.message.web.dto.MessageRequestDTO;
+import gaji.service.domain.user.entity.User;
+import gaji.service.domain.user.repository.UserRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class MessageCommandServiceImpl implements MessageCommandService{
+
+    UserRepository userRepository;
+
+    @Override
     public Message createMessage(Long myId,Long otherId, MessageRequestDTO.CreateDTO request) {
-        Message newMessage = MessageConverter.toMessage(myId,otherId,request);
+        Message sendMessage = MessageConverter.sendMessage(myId,otherId,request);
+        Message receiveMessage = MessageConverter.receiveMessage(myId,otherId,request);
+
+
         return null;
     }
 }
