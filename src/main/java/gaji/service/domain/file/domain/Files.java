f718376e2cc2b353d12431908eabf85c8313a0d3
shimfff <shimseawon1510@gmail.com>
2024-07-27 15:31:53 +0900

    :sparkles: Feat: 파일 엔티티 저장 로직 추가

--- src/main/java/gaji/service/domain/file/domain/Files.java
+++ src/main/java/gaji/service/domain/file/domain/Files.java
@@ -2,6 +2,7 @@ package gaji.service.domain.file.domain;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -24,12 +25,17 @@ public class Files {
     private String originalName;
 
     //원본이름
-    private String FileName;
+    private String fileName;
 
     //파일 경로
     private String path;
 
-
-
-
+    @Builder
+    public Files(String type, Integer size, String originalName, String fileName, String path) {
+        this.type = type;
+        this.size = size;
+        this.originalName = originalName;
+        this.fileName = fileName;
+        this.path = path;
+    }
 }
