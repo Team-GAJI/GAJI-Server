22256f4d3ae4acad0c103dc5935575ed34ecfb40
strongmhk <kim6562166086@gmail.com>
2024-07-27 12:02:19 +0900

    :sparkles: [#23] feature(PostStatusEnum): 역직렬화 메서드 구현
    요청값이 들어오면 PostStatusEnum의 value("모집중", "모집완료", ...)와 매칭되는지 확인하고,
    RECRUITING, RECRUITMENT_COMPLETED, NEED_RESOLUTION, RESOLVED, DELETED, POSTING중 매칭되는 값을 저장

--- src/main/java/gaji/service/domain/enums/PostStatusEnum.java
+++ src/main/java/gaji/service/domain/enums/PostStatusEnum.java
@@ -1,5 +1,35 @@
 package gaji.service.domain.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Slf4j
 public enum PostStatusEnum {
-    RECRUITING, RECRUITMENT_COMPLETED, NEED_RESOLUTION, RESOLVED, DELETED
+    RECRUITING("모집중"),
+    RECRUITMENT_COMPLETED("모집완료"),
+    NEED_RESOLUTION("해결필요"),
+    RESOLVED("해결완료"),
+    DELETED("삭제됨"),
+    POSTING("게시중");
+
+    @JsonValue
+    private final String value;
+
+    PostStatusEnum(String value) {
+        this.value = value;
+    }
+
+    @JsonCreator // Json -> Object, 역직렬화 수행하는 메서드
+    public static PostStatusEnum from(String param) {
+        for (PostStatusEnum postStatusEnum : PostStatusEnum.values()) {
+            if (postStatusEnum.getValue().equals(param)) {
+                return postStatusEnum;
+            }
+        }
+        log.error("PostStatusEnum.from() exception occur param: {}", param);
+        return null;
+    }
 }
