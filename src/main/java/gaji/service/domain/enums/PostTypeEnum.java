8328d1632f5b92d913f1fb8dd8ef0fe742ed4a42
strongmhk <kim6562166086@gmail.com>
2024-07-27 11:57:37 +0900

    :sparkles: [#23] feature(PostTypeEnum): 역직렬화 메서드 구현
    요청값이 들어오면 PostTypeEnum의 value("블로그", "프로젝트 모집", "질문")과 매칭되는지 확인하고,
    BLOG, PROJECT_RECRUITMENT, QUESTION중 매칭되는 값을 저장`

--- src/main/java/gaji/service/domain/enums/PostTypeEnum.java
+++ src/main/java/gaji/service/domain/enums/PostTypeEnum.java
@@ -1,6 +1,33 @@
 package gaji.service.domain.enums;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Slf4j
 public enum PostTypeEnum {
-    BLOG, PROJECT_RECRUITMENT, QUESTION
+    BLOG("블로그"),
+    PROJECT_RECRUITMENT("프로젝트 모집"),
+    QUESTION("질문");
+
+    @JsonValue
+    private final String value;
+
+    PostTypeEnum(String value) {
+        this.value = value;
+    }
+
+    @JsonCreator // Json -> Object, 역직렬화 수행하는 메서드
+    public static PostTypeEnum from(String param) {
+        for (PostTypeEnum postTypeEnum : PostTypeEnum.values()) {
+            if (postTypeEnum.getValue().equals(param)) {
+                return postTypeEnum;
+            }
+        }
+        log.error("PostTypeEnum.from() exception occur param: {}", param);
+        return null;
+    }
 
 }
