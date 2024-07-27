6ab2ac40203083c52446951482505ed930faed8b
karryred <ujin037@gmail.com>
2024-07-28 04:46:34 +0900

    :sparkles: Feat: MessageBodyConverter 생성

---
+++ src/main/java/gaji/service/domain/message/converter/MessageBodyConverter.java
@@ -0,0 +1,13 @@
+package gaji.service.domain.message.converter;
+
+import gaji.service.domain.message.entity.Message;
+import gaji.service.domain.message.entity.MessageBody;
+import gaji.service.domain.message.web.dto.MessageRequestDTO;
+
+public class MessageBodyConverter {
+    public static MessageBody toMessageBody(MessageRequestDTO.CreateDTO messageRequestDTO){
+        return MessageBody.builder()
+                .body(messageRequestDTO.getBody())
+                .build();
+    }
+}
