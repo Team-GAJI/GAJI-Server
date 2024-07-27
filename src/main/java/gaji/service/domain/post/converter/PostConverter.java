891282615719baca6499677c4c557f563d706f2e
strongmhk <kim6562166086@gmail.com>
2024-07-27 12:16:37 +0900

    [#23] feature(PostConverter): Post 변환 메서드 구현

---
+++ src/main/java/gaji/service/domain/post/converter/PostConverter.java
@@ -0,0 +1,26 @@
+package gaji.service.domain.post.converter;
+
+import gaji.service.domain.User;
+import gaji.service.domain.enums.PostStatusEnum;
+import gaji.service.domain.enums.PostTypeEnum;
+import gaji.service.domain.post.entity.Post;
+import gaji.service.domain.post.web.dto.PostRequestDTO;
+
+public class PostConverter {
+
+    public static PostStatusEnum getInitialPostStatus(PostTypeEnum type) {
+        return (type == PostTypeEnum.QUESTION) ? PostStatusEnum.NEED_RESOLUTION :
+                (type == PostTypeEnum.PROJECT_RECRUITMENT) ? PostStatusEnum.RECRUITING : PostStatusEnum.POSTING;
+    }
+
+    public static Post toPost(PostRequestDTO.UploadPostDTO request, User user) {
+        return Post.builder()
+                .user(user)
+                .title(request.getTitle())
+                .body(request.getBody())
+                .type(request.getType())
+                .status(getInitialPostStatus(request.getType()))
+                .build();
+    }
+
+}
