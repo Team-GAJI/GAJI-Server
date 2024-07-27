bc81c9ca261123323be0c685b9897f38237594c4
strongmhk <kim6562166086@gmail.com>
2024-07-27 12:08:31 +0900

    🔧 [#23] chore: common 패키지 구조 수정
    여러 도메인에서 공통으로 사용되는 클래스는 common 패키지에서 관리합니다.
    그래서 아래와 같은 변경 사항을 적용했습니다.
    
    1. BaseEntity, Hashtag, SelectHashtag를 기존 common에서 common/entity로 이동

---
+++ src/main/java/gaji/service/domain/common/entity/Hashtag.java
@@ -0,0 +1,29 @@
+package gaji.service.domain.common.entity;
+
+import gaji.service.domain.enums.PostTypeEnum;
+import gaji.service.domain.recruite.SelectCategory;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Hashtag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 30)
+    private String name;
+
+    @Builder
+    public Hashtag(String name) {
+        this.name = name;
+    }
+}
