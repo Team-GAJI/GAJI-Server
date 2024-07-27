0e721bf2d845eb38035f4c85d96c7e6304062ae9
strongmhk <kim6562166086@gmail.com>
2024-07-27 12:25:02 +0900

    :sparkles: [#23] feature(SelectHashtagRepository): Repository 생성

---
+++ src/main/java/gaji/service/domain/common/repository/SelectHashtagRepository.java
@@ -0,0 +1,7 @@
+package gaji.service.domain.common.repository;
+
+import gaji.service.domain.common.entity.SelectHashtag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SelectHashtagRepository extends JpaRepository<SelectHashtag, Long> {
+}
