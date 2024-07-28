22c2ab0ab23109428338165771d79f22ca856b1b
karryred <ujin037@gmail.com>
2024-07-29 00:53:18 +0900

    :recycle: Refactor: MessageDTO 수정

--- src/main/java/gaji/service/domain/message/web/dto/MessageResponseDTO.java
+++ src/main/java/gaji/service/domain/message/web/dto/MessageResponseDTO.java
@@ -1,12 +1,19 @@
 package gaji.service.domain.message.web.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 public class MessageResponseDTO {
+
     @Builder
     @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
     public static class CreateResultDTO{
-        Long messageId;
+        List<Long> messageIds;
     }
 }
