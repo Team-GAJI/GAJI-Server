86e5c882e50bed2212707ee6a95a08820467eafa
karryred <ujin037@gmail.com>
2024-07-29 01:31:16 +0900

    :sparkles: Feat: MessageCommandService 생성

--- src/main/java/gaji/service/domain/message/service/MessageCommandServiceImpl.java
+++ src/main/java/gaji/service/domain/message/service/MessageCommandServiceImpl.java
@@ -1,23 +1,54 @@
 package gaji.service.domain.message.service;
 
-import gaji.service.domain.message.Message;
+import gaji.service.domain.message.converter.MessageBodyConverter;
+import gaji.service.domain.message.entity.Message;
 import gaji.service.domain.message.converter.MessageConverter;
+import gaji.service.domain.message.entity.MessageBody;
+import gaji.service.domain.message.repository.MessageBodyRepository;
+import gaji.service.domain.message.repository.MessageRepository;
 import gaji.service.domain.message.web.dto.MessageRequestDTO;
+import gaji.service.domain.user.code.UserErrorStatus;
 import gaji.service.domain.user.entity.User;
 import gaji.service.domain.user.repository.UserRepository;
+import gaji.service.global.exception.RestApiException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class MessageCommandServiceImpl implements MessageCommandService{
 
-    UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final MessageRepository messageRepository;
+    private final MessageBodyRepository messageBodyRepository;
+
 
     @Override
-    public Message createMessage(Long myId,Long otherId, MessageRequestDTO.CreateDTO request) {
-        Message sendMessage = MessageConverter.sendMessage(myId,otherId,request);
-        Message receiveMessage = MessageConverter.receiveMessage(myId,otherId,request);
+    @Transactional
+    public List<Message> createMessage(Long myId, Long otherId, MessageRequestDTO.CreateDTO request) {
 
+        User mine = userRepository.findById(myId)
+                .orElseThrow(()-> new RestApiException(UserErrorStatus._USER_NOT_FOUND));
+        User other = userRepository.findById(otherId)
+                .orElseThrow(()-> new RestApiException(UserErrorStatus._USER_NOT_FOUND));
 
-        return null;
+        Message sendMessage = MessageConverter.toSendMessage(mine,other);
+        Message receiveMessage = MessageConverter.toReceiveMessage(mine, other);
+
+        MessageBody messageBody = MessageBodyConverter.toMessageBody(request);
+
+        //연관관계 매핑을 위해 setter 사용
+        sendMessage.setMessageBody(messageBody);
+        receiveMessage.setMessageBody(messageBody);
+
+        messageRepository.save(sendMessage);
+        messageRepository.save(receiveMessage);
+        messageBodyRepository.save(messageBody);
+
+        return Arrays.asList(sendMessage, receiveMessage);
     }
 }
