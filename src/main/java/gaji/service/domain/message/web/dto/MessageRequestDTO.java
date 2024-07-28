9040c737e46a3c7cd5937777ad756ea3f0a3e757
karryred <ujin037@gmail.com>
2024-07-29 00:53:18 +0900

    :recycle: Refactor: MessageDTO 수정

--- src/main/java/gaji/service/domain/message/web/dto/MessageRequestDTO.java
+++ src/main/java/gaji/service/domain/message/web/dto/MessageRequestDTO.java
@@ -1,11 +1,15 @@
 package gaji.service.domain.message.web.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 public class MessageRequestDTO {
 
     @Getter
+    @RequiredArgsConstructor
     public static class CreateDTO{
-        private String body;
+        @NotBlank(message = "쪽지 내용을 입력해주세요.")
+        private final String body;
     }
 }
