29f03963187ca10a70eb3ad66940952678719f8f
karryred <ujin037@gmail.com>
2024-07-29 00:51:54 +0900

    :sparkles: Feat: UserConverter 생성

--- src/main/java/gaji/service/domain/user/converter/UserConverter.java
+++ src/main/java/gaji/service/domain/user/converter/UserConverter.java
@@ -1,12 +1,4 @@
 package gaji.service.domain.user.converter;
 
 public class UserConverter {
-
-    public static UserResponseDTO.LogoutResultDTO toLogoutResultDTO(User user){
-        return UserResponseDTO.LogoutResultDTO.builder()
-                .userId(user.getId())
-                .deletedAt(LocalDateTime.now())
-                .build();
-    }
-
 }
