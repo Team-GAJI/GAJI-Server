f818b45f6b42febec9c002922679363dfa35fff4
strongmhk <kim6562166086@gmail.com>
2024-07-29 09:36:10 +0900

    ✨ [#29] feature(PostCommandServiceImpl): `@Transactional` 추가

--- src/main/java/gaji/service/domain/post/service/PostCommandServiceImpl.java
+++ src/main/java/gaji/service/domain/post/service/PostCommandServiceImpl.java
@@ -15,12 +15,14 @@ import gaji.service.domain.user.repository.UserRepository;
 import gaji.service.global.exception.RestApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class PostCommandServiceImpl implements PostCommandService {
 
     private final UserRepository userRepository;
