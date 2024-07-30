926a817b04fc317411f812dfcdccf3e7e0b9dd63
mmingoo <aldrn0323@naver.com>
2024-07-31 03:44:16 +0900

    🔥 Remove: @ValiWeek와 검증 로직 삭제

--- src/main/java/gaji/service/domain/room/web/dto/RoomRequestDto.java
+++ src/main/java/gaji/service/domain/room/web/dto/RoomRequestDto.java
@@ -1,16 +1,12 @@
 package gaji.service.domain.room.web.dto;
 
-import gaji.service.domain.room.validation.annotation.ValidWeek;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
