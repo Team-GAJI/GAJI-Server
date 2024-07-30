58ed65b24869456613f471254511a8dea2586d77
mmingoo <aldrn0323@naver.com>
2024-07-30 16:19:45 +0900

    ♻️ Assignment 컨트롤러 반환 타입 변경

--- src/main/java/gaji/service/domain/room/web/controller/AssignmentController.java
+++ src/main/java/gaji/service/domain/room/web/controller/AssignmentController.java
@@ -3,11 +3,10 @@ package gaji.service.domain.room.web.controller;
 import gaji.service.domain.room.service.RoomAssignmentServiceImpl;
 import gaji.service.domain.room.web.dto.AssignmentRequestDto;
 import gaji.service.domain.room.web.dto.AssignmentResponseDto;
+import gaji.service.global.base.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,10 +16,10 @@ public class AssignmentController {
 
     private final RoomAssignmentServiceImpl assignmentService;
 
-    @PostMapping("/assignments/{roomId}")
+    @PostMapping("/assignments/{userId}/{roomId}")
     @Operation(summary = "스터디룸 과제 등록 API",description = "스터디룸의 과제를 등록하는 API입니다. room의 id가 존재하는지, 스터디에 참혀하고 있는 user인지 검증합니다.")
-    public ResponseEntity<AssignmentResponseDto> AssignmentController(Long userId/*하드 코딩용, 추후 수정*/,@RequestBody @Valid AssignmentRequestDto.AssignmentDto requestDto, @PathVariable Long roomId){
+    public BaseResponse<AssignmentResponseDto> AssignmentController(@PathVariable Long userId/*하드 코딩용, 추후 수정*/,@RequestBody @Valid AssignmentRequestDto.AssignmentDto requestDto, @PathVariable Long roomId){
         AssignmentResponseDto responseDto = assignmentService.createAssignment(roomId, userId, requestDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+        return BaseResponse.onSuccess(responseDto);
     }
 }
