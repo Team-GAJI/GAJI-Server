e05fbcb8ccfbc935aee1e0410440bd28bd24cb25
strongmhk <kim6562166086@gmail.com>
2024-07-30 10:49:24 +0900

    :sparkles: [#29] feature(PostConverter): DTO -> Comment 변환 메서드 구현

--- src/main/java/gaji/service/domain/post/converter/PostConverter.java
+++ src/main/java/gaji/service/domain/post/converter/PostConverter.java
@@ -3,11 +3,13 @@ package gaji.service.domain.post.converter;
 import gaji.service.domain.User;
 import gaji.service.domain.enums.PostStatusEnum;
 import gaji.service.domain.enums.PostTypeEnum;
+import gaji.service.domain.post.entity.Comment;
 import gaji.service.domain.post.entity.Post;
 import gaji.service.domain.post.web.dto.PostRequestDTO;
 
 public class PostConverter {
 
+    // 초기 PostStatus 지정
     public static PostStatusEnum getInitialPostStatus(PostTypeEnum type) {
         return (type == PostTypeEnum.QUESTION) ? PostStatusEnum.NEED_RESOLUTION :
                 (type == PostTypeEnum.PROJECT_RECRUITMENT) ? PostStatusEnum.RECRUITING : PostStatusEnum.POSTING;
@@ -22,5 +24,15 @@ public class PostConverter {
                 .status(getInitialPostStatus(request.getType()))
                 .build();
     }
+
+    public static Comment toComment(PostRequestDTO.WriteCommentDTO request, User user, Post post, Comment parentComment) {
+        return Comment.builder()
+                .user(user)
+                .post(post)
+                .parent(parentComment)
+                .body(request.getBody())
+                .build();
+    }
+
 
 }
