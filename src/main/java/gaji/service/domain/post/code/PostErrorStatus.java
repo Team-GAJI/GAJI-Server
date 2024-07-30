1a4dee7d57808f2635a39a828aa04c105f14f4a9
strongmhk <kim6562166086@gmail.com>
2024-07-30 10:41:31 +0900

    :sparkles: [#29] feature(PostErrorStatus): 400 에러 추가

--- src/main/java/gaji/service/domain/post/code/PostErrorStatus.java
+++ src/main/java/gaji/service/domain/post/code/PostErrorStatus.java
@@ -11,8 +11,12 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum PostErrorStatus implements BaseErrorCodeInterface {
 
-    _POST_TYPE_NOT_FOUND(HttpStatus.BAD_REQUEST, "POST_4001", "존재하지 않는 게시글 유형입니다."),
+    _POST_NOT_FOUND(HttpStatus.BAD_REQUEST, "POST_4001", "존재하지 않는 게시글입니다."),
+    _POST_TYPE_NOT_FOUND(HttpStatus.BAD_REQUEST, "POST_4002", "존재하지 않는 게시글 유형입니다."),
+    _COMMENT_NOT_FOUND(HttpStatus.BAD_REQUEST, "POST_4003", "존재하지 않는 댓글입니다."),
+
     _USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "USER_4001", "존재하지 않는 회원입니다."), // user 도메인을 건드리지 않기 위해 생성한 임시 에러코드
+
     ;
 
     private final HttpStatus httpStatus;
