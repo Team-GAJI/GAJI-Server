4d8b7cfd00852759826c9c3c05b8523d1ed35702
spenshark <xzx01299@naver.com>
2024-07-31 00:27:11 +0900

    ✨ [#28] feature(RoomConverter): Room 객체의 변환을 위한 Converter 구현

---
+++ src/main/java/gaji/service/domain/room/converter/RoomConverter.java
@@ -0,0 +1,39 @@
+package gaji.service.domain.room.converter;
+
+import gaji.service.domain.curriculum.entity.Curriculum;
+import gaji.service.domain.room.entity.Material;
+import gaji.service.domain.room.entity.Room;
+import gaji.service.domain.room.entity.Way;
+import gaji.service.domain.room.web.dto.RoomRequestDTO;
+import gaji.service.domain.room.web.dto.RoomResponseDTO;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class RoomConverter {
+
+    public static Room toRoom(RoomRequestDTO.CreateStudyDTO request, String ThumbnailPath, List<Material> materialList, Curriculum curriculum, Way way) {
+
+        return Room.builder()
+                .name(request.getName())
+                .description(request.getDescription())
+                .thumbnailPath(ThumbnailPath)
+                .materialList(materialList)
+                .headCount(request.getHeadCount())
+                .isPrivate(request.isPrivate())
+                .recruitStartDay(request.getRecruitStartDay())
+                .recruitEndDay(request.getRecruitEndDay())
+                .studyStartDay(request.getStudyStartDay())
+                .studyEndDay(request.getStudyEndDay())
+                .curriculum(curriculum)
+                .way(way)
+                .build();
+    }
+
+    public static RoomResponseDTO.CreateStudyDTO toResponseDTO(Room room) {
+        return RoomResponseDTO.CreateStudyDTO.builder()
+                .roomId(room.getId())
+                .build();
+    }
+}
