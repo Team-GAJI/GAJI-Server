15feeab7b863eb82b5094dcad30a0e44e8f9f764
mmingoo <aldrn0323@naver.com>
2024-07-31 04:24:42 +0900

    ♻️ RoomErrorStatus 에러 코드 수정

--- src/main/java/gaji/service/domain/room/code/RoomErrorStatus.java
+++ src/main/java/gaji/service/domain/room/code/RoomErrorStatus.java
@@ -17,10 +17,10 @@ public enum RoomErrorStatus implements BaseErrorCodeInterface {
     _WEEK_NOT_VALID(HttpStatus.BAD_REQUEST, "ROOM_4002", "유효하지 않은 형식의 주차입니다."),
 
     // 스터디룸 멤버 관련 오류
-    _USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "ROOM_4002","사용자를 찾을 수 없습니다."),
-    _USER_NOT_IN_ROOM(HttpStatus.BAD_REQUEST, "ROOM_4002","사용자가 해당 스터디룸에 참여하고 있지 않습니다."),
+    _USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "ROOM_4003","사용자를 찾을 수 없습니다."),
+    _USER_NOT_IN_ROOM(HttpStatus.BAD_REQUEST, "ROOM_4004","사용자가 해당 스터디룸에 참여하고 있지 않습니다."),
 
-    _USER_NOT_READER_IN_ROOM(HttpStatus.BAD_REQUEST, "ROOM_4002","사용자가 해당 스터디룸에 참여하고 있지 않습니다.");
+    _USER_NOT_READER_IN_ROOM(HttpStatus.BAD_REQUEST, "ROOM_4005","사용자가 해당 스터디룸에 참여하고 있지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final boolean isSuccess = false;
