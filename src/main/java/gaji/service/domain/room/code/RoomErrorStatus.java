f744a2921c900ba1248d33b99fe6104348128203
mmingoo <aldrn0323@naver.com>
2024-07-31 00:20:50 +0900

    ♻️ RoomErrorStatus에서 에러 유형 수정

--- src/main/java/gaji/service/domain/room/code/RoomErrorStatus.java
+++ src/main/java/gaji/service/domain/room/code/RoomErrorStatus.java
@@ -20,6 +20,7 @@ public enum RoomErrorStatus implements BaseErrorCodeInterface {
     _USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "ROOM_4003","사용자를 찾을 수 없습니다."),
     _USER_NOT_IN_ROOM(HttpStatus.BAD_REQUEST, "ROOM_4004","사용자가 해당 스터디룸에 참여하고 있지 않습니다.");
 
+
     private final HttpStatus httpStatus;
     private final boolean isSuccess = false;
     private final String code;
