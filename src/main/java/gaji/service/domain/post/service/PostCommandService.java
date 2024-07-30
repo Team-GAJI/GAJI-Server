d525df1224862e673bb1134d4e6974a3a7189643
strongmhk <kim6562166086@gmail.com>
2024-07-30 11:04:15 +0900

    :sparkles: [#29] feature: 커뮤니티 게시글 삭제, 댓글 작성 및 삭제 비즈니스 로직 구현

--- src/main/java/gaji/service/domain/post/service/PostCommandService.java
+++ src/main/java/gaji/service/domain/post/service/PostCommandService.java
@@ -1,10 +1,13 @@
 package gaji.service.domain.post.service;
 
+import gaji.service.domain.post.entity.Comment;
 import gaji.service.domain.post.entity.Post;
 import gaji.service.domain.post.web.dto.PostRequestDTO;
 
 public interface PostCommandService {
 
     Post uploadPost(Long userId, PostRequestDTO.UploadPostDTO request);
-
+    Comment writeCommentOnCommunityPost(Long userId, Long postId, Long parentCommentId, PostRequestDTO.WriteCommentDTO request);
+    void softDeleteComment(Long commentId);
+    void hardDeleteCommunityPost(Long postId);
 }
--- src/main/java/gaji/service/domain/post/service/PostCommandServiceImpl.java
+++ src/main/java/gaji/service/domain/post/service/PostCommandServiceImpl.java
@@ -6,9 +6,12 @@ import gaji.service.domain.common.entity.Hashtag;
 import gaji.service.domain.common.entity.SelectHashtag;
 import gaji.service.domain.common.repository.HashtagRepository;
 import gaji.service.domain.common.repository.SelectHashtagRepository;
+import gaji.service.domain.enums.CommentStatus;
 import gaji.service.domain.post.code.PostErrorStatus;
 import gaji.service.domain.post.converter.PostConverter;
+import gaji.service.domain.post.entity.Comment;
 import gaji.service.domain.post.entity.Post;
+import gaji.service.domain.post.repository.CommentRepository;
 import gaji.service.domain.post.repository.PostRepository;
 import gaji.service.domain.post.web.dto.PostRequestDTO;
 import gaji.service.domain.user.repository.UserRepository;
@@ -29,25 +32,59 @@ public class PostCommandServiceImpl implements PostCommandService {
     private final PostRepository postRepository;
     private final HashtagRepository hashtagRepository;
     private final SelectHashtagRepository selectHashtagRepository;
+    private final CommentRepository commentRepository;
 
     @Override
     public Post uploadPost(Long userId, PostRequestDTO.UploadPostDTO request) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new RestApiException(PostErrorStatus._USER_NOT_FOUND));
-        Post post = PostConverter.toPost(request, user);
+        User findUser = findUserByUserId(userId);
+        Post post = PostConverter.toPost(request, findUser);
         Post newPost = postRepository.save(post);
 
-        List<String> hashtagStringList = request.getHashtagList();
-        List<Hashtag> hashtagEntityList = getHashtagListByhashtagStringList(hashtagStringList);
+        if (request.getHashtagList() != null) {
+            List<String> hashtagStringList = request.getHashtagList();
+            List<Hashtag> hashtagEntityList = convertHashtagStringListToHashtagList(hashtagStringList);
 
-        List<SelectHashtag> selectHashtagList = HashtagConverter.toSelectHashtagList(hashtagEntityList, post.getId(), request.getType());
-        selectHashtagRepository.saveAll(selectHashtagList);
-
+            List<SelectHashtag> selectHashtagList = HashtagConverter.toSelectHashtagList(hashtagEntityList, post.getId(), request.getType());
+            selectHashtagRepository.saveAll(selectHashtagList);
+        }
         return newPost;
     }
 
+
+    @Override
+    public Comment writeCommentOnCommunityPost(Long userId, Long postId, Long parentCommentId, PostRequestDTO.WriteCommentDTO request) {
+        User findUser = findUserByUserId(userId);
+        Post findPost = findPostByPostId(postId);
+
+        Comment newComment = createCommentByCheckParentCommentIdIsNull(parentCommentId, request, findUser, findPost);
+        return commentRepository.save(newComment);
+    }
+
+    @Override
+    public void softDeleteComment(Long commentId) {
+        Comment findComment = findCommentByCommentId(commentId);
+        findComment.updateStatus(CommentStatus.DELETE);
+    }
+
+    // TODO: 게시글 파일도 함께 삭제
+    @Override
+    public void hardDeleteCommunityPost(Long postId) {
+        Post findPost = findPostByPostId(postId);
+        selectHashtagRepository.deleteAllByEntityIdAndType(findPost.getId(), findPost.getType());
+        postRepository.delete(findPost);
+    }
+
+    private Comment createCommentByCheckParentCommentIdIsNull(Long parentCommentId, PostRequestDTO.WriteCommentDTO request, User findUser, Post findPost) {
+        if (parentCommentId != null) {
+            Comment parentComment = findCommentByCommentId(parentCommentId);
+            return PostConverter.toComment(request, findUser, findPost, parentComment);
+        } else {
+            return PostConverter.toComment(request, findUser, findPost, null);
+        }
+    }
+
     // 이미 존재하는 해시태그는 조회, 존재하지 않는 해시태그는 생성해서 List로 반환하는 메서드
-    private List<Hashtag> getHashtagListByhashtagStringList(List<String> hashtagStringList) {
+    private List<Hashtag> convertHashtagStringListToHashtagList(List<String> hashtagStringList) {
         return hashtagStringList.stream()
                 .map(hashtag -> {
                     if (hashtagRepository.existsByName(hashtag)) {
@@ -57,5 +94,20 @@ public class PostCommandServiceImpl implements PostCommandService {
                     }
                 })
                 .collect(Collectors.toList());
+    }
+
+    private User findUserByUserId(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new RestApiException(PostErrorStatus._USER_NOT_FOUND));
+    }
+
+    private Post findPostByPostId(Long postId) {
+        return postRepository.findById(postId)
+                .orElseThrow(() -> new RestApiException(PostErrorStatus._POST_NOT_FOUND));
+    }
+
+    private Comment findCommentByCommentId(Long parentCommentId) {
+        return commentRepository.findById(parentCommentId)
+                .orElseThrow(() -> new RestApiException(PostErrorStatus._COMMENT_NOT_FOUND));
     }
 }
