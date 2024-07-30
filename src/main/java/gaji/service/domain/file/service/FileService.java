bbf33b3223eb04c45e562ba2726af2f2ff160a2e
shimfff <shimseawon1510@gmail.com>
2024-07-30 14:37:18 +0900

    :sparkles: Feat: FilesDeleteResponse 추가

--- src/main/java/gaji/service/domain/file/service/FileService.java
+++ src/main/java/gaji/service/domain/file/service/FileService.java
@@ -1,6 +1,7 @@
 package gaji.service.domain.file.service;
 
 import gaji.service.domain.file.dto.response.FileCreateResponse;
+import gaji.service.domain.file.dto.response.FileDeleteResponse;
 import gaji.service.global.common.enums.FileCategory;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -8,5 +9,5 @@ public interface FileService {
 
     FileCreateResponse createFile(FileCategory fIleCategory , final MultipartFile file);
 
-    void deleteFile(String fileUrl);
+    FileDeleteResponse deleteFile(String fileUrl);
 }
