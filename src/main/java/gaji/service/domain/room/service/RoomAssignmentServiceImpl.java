fc3c1fbc2f5515ba0ebb996c29e43983ca07aabe
mmingoo <aldrn0323@naver.com>
2024-07-30 11:12:46 +0900

    ♻️ RoommAssignmentImpl로 변수명 변경

--- src/main/java/gaji/service/domain/room/service/RoomAssignmentServiceImpl.java
+++ src/main/java/gaji/service/domain/room/service/RoomAssignmentServiceImpl.java
@@ -1,6 +1,5 @@
 package gaji.service.domain.room.service;
 
-import gaji.service.domain.User;
 import gaji.service.domain.room.code.PostErrorStatus;
 import gaji.service.domain.room.entity.Room;
 import gaji.service.domain.room.repository.AssignmentRepository;
@@ -11,13 +10,12 @@ import gaji.service.domain.studyMate.Assignment;
 import gaji.service.domain.studyMate.repository.StudyMateRepository;
 import gaji.service.domain.user.repository.UserRepository;
 import gaji.service.global.exception.RestApiException;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class PostAssignmentServiceImpl implements PostAssignmentService {
+public class RoomAssignmentServiceImpl implements RoomAssignmentService {
     private final RoomRepository roomRepository;
     private final AssignmentRepository assignmentRepository;
     private final UserRepository userRepository;
