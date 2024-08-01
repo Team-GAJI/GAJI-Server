b72bb667741485acc8b2eff22c34cdf4ed332354
spenshark <xzx01299@naver.com>
2024-08-01 11:44:08 +0900

    ✨ [#43] feature: 엔티티 생성자 메소드 구현 및 초기값 설정

--- src/main/java/gaji/service/domain/recruit/entity/SelectCategory.java
+++ src/main/java/gaji/service/domain/recruit/entity/SelectCategory.java
@@ -1,7 +1,9 @@
 package gaji.service.domain.recruit.entity;
 
+import gaji.service.domain.enums.RecruitPostCategoryEnum;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -17,7 +19,13 @@ public class SelectCategory {
     @JoinColumn(name = "post_id")
     private RecruitPost recruitPost;
 
-    @ManyToOne(fetch =  FetchType.LAZY)
-    @JoinColumn(name = "category_id")
-    private Category category;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "category")
+    private RecruitPostCategoryEnum category;
+
+    @Builder
+    public SelectCategory(RecruitPost recruitPost, RecruitPostCategoryEnum category) {
+        this.recruitPost = recruitPost;
+        this.category = category;
+    }
 }
