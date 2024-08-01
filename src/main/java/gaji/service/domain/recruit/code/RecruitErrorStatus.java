47f0985866efb01e85800fbbb7d2e62a50c141a8
spenshark <xzx01299@naver.com>
2024-08-01 11:54:01 +0900

    ✨ [#43] feature(RecruitErrorStatus): Recruit 관련 에러들을 관리하는 Enum 타입 구현
    UserErrorStatus 생성 전 임의로 유저 관련 에러 구현

---
+++ src/main/java/gaji/service/domain/recruit/code/RecruitErrorStatus.java
@@ -0,0 +1,31 @@
+package gaji.service.domain.recruit.code;
+
+import gaji.service.global.exception.code.BaseCodeDto;
+import gaji.service.global.exception.code.BaseErrorCodeInterface;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+
+@Getter
+@AllArgsConstructor
+public enum RecruitErrorStatus implements BaseErrorCodeInterface {
+    _USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "USER_4001","사용자를 찾을 수 없습니다."), // 임시 생성
+    _RECRUIT_CATEGORY_NOT_FOUND(HttpStatus.BAD_REQUEST, "RECRUIT_4001", "해당 카테고리가 존재하지 않습니다.")
+    ;
+
+    private final HttpStatus httpStatus;
+    private final boolean isSuccess = false;
+    private final String code;
+    private final String message;
+
+    @Override
+    public BaseCodeDto getErrorCode() {
+        return BaseCodeDto.builder()
+                .httpStatus(httpStatus)
+                .isSuccess(isSuccess)
+                .code(code)
+                .message(message)
+                .build();
+    }
+}
