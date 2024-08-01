fb03127afb0e24df61294a4b258eae32634e9037
spenshark <xzx01299@naver.com>
2024-08-01 13:03:44 +0900

    ✨ [#43] feature(RecruitErrorStatus): 해당 모집 게시글을 찾을 수 없을 때의 에러코드 추가

--- src/main/java/gaji/service/domain/recruit/code/RecruitErrorStatus.java
+++ src/main/java/gaji/service/domain/recruit/code/RecruitErrorStatus.java
@@ -11,7 +11,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum RecruitErrorStatus implements BaseErrorCodeInterface {
     _USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "USER_4001","사용자를 찾을 수 없습니다."), // 임시 생성
-    _RECRUIT_CATEGORY_NOT_FOUND(HttpStatus.BAD_REQUEST, "RECRUIT_4001", "해당 카테고리가 존재하지 않습니다.")
+    _RECRUIT_CATEGORY_NOT_FOUND(HttpStatus.BAD_REQUEST, "RECRUIT_4001", "해당 카테고리가 존재하지 않습니다."),
+    _RECRUIT_POST_NOT_FOUND(HttpStatus.NOT_FOUND, "RECRUIT_4002", "모집 게시글을 찾을 수 없습니다.")
     ;
 
     private final HttpStatus httpStatus;
