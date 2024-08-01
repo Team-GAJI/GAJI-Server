fc2a1f7424215f31a5f456d7c9ce1b751f7b672b
mmingoo <aldrn0323@naver.com>
2024-08-01 09:22:23 +0900

    ✨   requestDto, responseDto 생성

--- src/main/java/gaji/service/domain/room/web/dto/RoomResponseDto.java
+++ src/main/java/gaji/service/domain/room/web/dto/RoomResponseDto.java
@@ -1,5 +1,6 @@
 package gaji.service.domain.room.web.dto;
 
+import gaji.service.domain.enums.PostTypeEnum;
 import gaji.service.domain.studyMate.Assignment;
 import lombok.*;
 
@@ -14,5 +15,16 @@ public class RoomResponseDto {
         Integer weeks;
         String body;
     }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class BoardDto{
+        String title;
+        String body;
+        PostTypeEnum postTypeEnum;
+    }
+
 
 }
