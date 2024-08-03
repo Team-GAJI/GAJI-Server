ce8a392749933ee26926f9ef938c52a972681699
spenshark <xzx01299@naver.com>
2024-08-04 00:21:04 +0900

    ✨ [#43] feature: 스터디 상세 조회 API 댓글 리스트 비즈니스 로직 구현
    상위 4개의 값만 표시하도록 구현

--- src/main/java/gaji/service/domain/recruit/service/RecruitQueryServiceImpl.java
+++ src/main/java/gaji/service/domain/recruit/service/RecruitQueryServiceImpl.java
@@ -4,6 +4,8 @@ import gaji.service.domain.User;
 import gaji.service.domain.enums.RoomCategoryEnum;
 import gaji.service.domain.recruit.code.RecruitErrorStatus;
 import gaji.service.domain.recruit.converter.RecruitConverter;
+import gaji.service.domain.recruit.entity.StudyComment;
+import gaji.service.domain.recruit.repository.StudyCommentRepository;
 import gaji.service.domain.recruit.web.dto.RecruitResponseDTO;
 import gaji.service.domain.room.entity.Room;
 import gaji.service.domain.room.repository.RoomRepository;
@@ -13,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Service
@@ -21,23 +24,36 @@ public class RecruitQueryServiceImpl implements RecruitQueryService {
 
     private final UserRepository userRepository;
     private final RoomRepository roomRepository;
+    private final StudyCommentRepository studyCommentRepository;
 
     @Override
     @Transactional(readOnly = true)
     public RecruitResponseDTO.studyDetailDTO getStudyDetail(Long roomId) {
+
         Room room = roomRepository.findById(roomId)
                 .orElseThrow(() -> new RestApiException(RecruitErrorStatus._RECRUIT_POST_NOT_FOUND));
 
         User user = userRepository.findById(room.getUser().getId())
                 .orElseThrow(() -> new RestApiException(RecruitErrorStatus._USER_NOT_FOUND));
 
+        List<StudyComment> commentList = studyCommentRepository.findByRoomAndDepth(room, 0);
+        List<RecruitResponseDTO.CommentResponseDTO> CommentResponseDTO;
+
+        if (commentList.isEmpty()) {
+            CommentResponseDTO = null;
+        } else {
+            commentList.sort(Comparator.comparing(StudyComment::getCreatedAt).reversed());
+            CommentResponseDTO = RecruitConverter.toCommentResponseDTOList(commentList);
+        }
+
+
         room.addView();
-        System.out.println(room.getViews());
         roomRepository.save(room);
 
         List<RoomCategoryEnum> categoryList = RecruitConverter.toCategoryList(room.getSelectCategoryList());
 
-        return RecruitConverter.toStudyDetailDTO(user, room, categoryList);
+        return RecruitConverter.toStudyDetailDTO(user, room, categoryList, commentList.size(), CommentResponseDTO);
     }
+
 
 }
