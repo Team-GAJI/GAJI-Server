df393b584a6ca4563d88e44810d3835cce4b7fbf
spenshark <xzx01299@naver.com>
2024-08-04 00:21:04 +0900

    ✨ [#43] feature: 스터디 상세 조회 API 댓글 리스트 비즈니스 로직 구현
    상위 4개의 값만 표시하도록 구현

--- src/main/java/gaji/service/domain/recruit/service/RecruitQueryService.java
+++ src/main/java/gaji/service/domain/recruit/service/RecruitQueryService.java
@@ -3,5 +3,5 @@ package gaji.service.domain.recruit.service;
 import gaji.service.domain.recruit.web.dto.RecruitResponseDTO;
 
 public interface RecruitQueryService {
-    RecruitResponseDTO.studyDetailDTO getStudyDetail(Long postId);
+    RecruitResponseDTO.studyDetailDTO getStudyDetail(Long roomId);
 }
