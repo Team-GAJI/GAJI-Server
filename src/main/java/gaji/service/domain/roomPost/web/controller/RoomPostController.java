a67444ca075cac91efa6a91bb27d02c396941446
mmingoo <aldrn0323@naver.com>
2024-08-03 14:49:40 +0900

    🔥 roomPostBoard 컨트롤러 삭제

--- src/main/java/gaji/service/domain/roomPost/web/controller/RoomPostController.java
+++ src/main/java/gaji/service/domain/roomPost/web/controller/RoomPostController.java
@@ -14,17 +14,6 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/studyRooms")
 public class RoomPostController {
 
-    private final RoomPostCommandServiceImpl roomBoardCommandService;
-
-    @PostMapping(value = "/post/{roomId}/{userId}")
-    @Operation(summary = "스터디룸 게시글 등록 API", description = "스터디룸의 게시글을 등록하는 API입니다. room의 id가 존재하는지, 스터디에 참여하고 있는 user인지 검증합니다.")
-    public BaseResponse<Long> RoomBoardController(
-            @PathVariable Long userId,
-            @PathVariable Long roomId,
-            @RequestBody @Valid RoomPostRequestDto.RoomPostDto requestDto) {
-        RoomPost roomPost = roomBoardCommandService.createRoomPost(roomId, userId, requestDto);
-        return BaseResponse.onSuccess(roomPost.getId());
-    }
 }
 
 
