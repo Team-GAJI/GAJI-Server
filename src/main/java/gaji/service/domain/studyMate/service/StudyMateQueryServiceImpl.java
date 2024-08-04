b39bc6dcb20adc7e7ccc55d3dcc5bbe7f4958ded
mmingoo <aldrn0323@naver.com>
2024-08-04 12:50:42 +0900

    ♻️  StudyMate의 service 의존성 수정

---
+++ src/main/java/gaji/service/domain/studyMate/service/StudyMateQueryServiceImpl.java
@@ -0,0 +1,23 @@
+package gaji.service.domain.studyMate.service;
+
+import gaji.service.domain.studyMate.StudyMate;
+import gaji.service.domain.studyMate.code.StudyMateErrorStatus;
+import gaji.service.domain.studyMate.repository.StudyMateRepository;
+import gaji.service.global.exception.RestApiException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StudyMateQueryServiceImpl implements StudyMateQueryService {
+
+    private final StudyMateRepository studyMateRepository;
+
+    @Override
+    public StudyMate findByUserIdAndRoomId(Long userId, Long roomId) {
+        return studyMateRepository.findByUserIdAndRoomId(userId, roomId)
+                .orElseThrow(() -> new RestApiException(StudyMateErrorStatus._USER_NOT_IN_STUDYROOM));
+
+    }
+
+}
