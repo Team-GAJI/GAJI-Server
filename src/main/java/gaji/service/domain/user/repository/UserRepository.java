b527142c25ef55ac350f41898c03dbcbb5ddbee4
karryred <ujin037@gmail.com>
2024-08-04 17:04:34 +0900

    :sparkles: Feat: UserRepository 메서드 추가

--- src/main/java/gaji/service/domain/user/repository/UserRepository.java
+++ src/main/java/gaji/service/domain/user/repository/UserRepository.java
@@ -3,9 +3,11 @@ package gaji.service.domain.user.repository;
 import gaji.service.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(Long id);
     User findByUsernameId(String username);
+    void deleteAllByInactiveTimeBefore(LocalDateTime time);
 }
