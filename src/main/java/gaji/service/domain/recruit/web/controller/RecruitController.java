4df8db75d8bf0c177f779d9e2ed7d3ba87c9e724
= <xzx01299@naver.com>
2024-08-04 14:44:56 +0900

    🔨 [#43] fix(RecruitController): 토큰을 통해 userId를 가져오도록 수정

--- src/main/java/gaji/service/domain/recruit/web/controller/RecruitController.java
+++ src/main/java/gaji/service/domain/recruit/web/controller/RecruitController.java
@@ -7,6 +7,7 @@ import gaji.service.domain.recruit.web.dto.RecruitRequestDTO;
 import gaji.service.domain.recruit.web.dto.RecruitResponseDTO;
 import gaji.service.domain.room.entity.Room;
 import gaji.service.global.base.BaseResponse;
+import gaji.service.jwt.service.TokenProviderService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -18,20 +19,22 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/studyRecruitPosts")
 public class RecruitController {
 
+    private final TokenProviderService tokenProviderService;
     private final RecruitCommandService recruitCommandService;
     private final RecruitQueryService recruitQueryService;
 
     @PostMapping("")
     @Operation(summary = "스터디 모집 게시글 생성 API", description = "스터디 모집 게시글을 생성하는 API입니다.")
-    public BaseResponse<RecruitResponseDTO.CreateRoomDTO> createRoom(@RequestBody @Valid RecruitRequestDTO.CreateRoomDTO request, @RequestParam("user") Long userId) {
+    public BaseResponse<RecruitResponseDTO.CreateRoomDTO> createRoom(@RequestBody @Valid RecruitRequestDTO.CreateRoomDTO request, @RequestHeader("Authorization") String authorizationHeader) {
+        Long userId = tokenProviderService.getUserIdFromToken(authorizationHeader);
         Room newRoom = recruitCommandService.createRoom(request, userId);
         return BaseResponse.onSuccess(RecruitConverter.toResponseDTO(newRoom));
     }
 
-    @GetMapping("/{postId}")
+    @GetMapping("/{roomId}")
     @Operation(summary = "스터디 정보 상세 조회 API", description = "스터디 상세 정보를 조회하는 API입니다.")
-    public BaseResponse<RecruitResponseDTO.studyDetailDTO> getStudyDetail(@PathVariable Long postId) {
-        RecruitResponseDTO.studyDetailDTO responseDTO = recruitQueryService.getStudyDetail(postId);
+    public BaseResponse<RecruitResponseDTO.studyDetailDTO> getStudyDetail(@PathVariable Long roomId) {
+        RecruitResponseDTO.studyDetailDTO responseDTO = recruitQueryService.getStudyDetail(roomId);
         return BaseResponse.onSuccess(responseDTO);
     }
 }
