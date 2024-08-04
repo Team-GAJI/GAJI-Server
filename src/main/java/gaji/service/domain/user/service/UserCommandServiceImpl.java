e4fa46718910fd0837ec48efa01fa07431c50712
karryred <ujin037@gmail.com>
2024-08-04 17:01:17 +0900

    :sparkles: Feat: UserCommandService 와 Impl 구현

--- src/main/java/gaji/service/domain/user/service/UserCommandServiceImpl.java
+++ src/main/java/gaji/service/domain/user/service/UserCommandServiceImpl.java
@@ -1,7 +1,63 @@
 package gaji.service.domain.user.service;
 
+import gaji.service.domain.enums.UserActive;
+import gaji.service.domain.user.code.UserErrorStatus;
+import gaji.service.domain.user.entity.User;
+import gaji.service.domain.user.enums.UserDeletePeriod;
+import gaji.service.domain.user.repository.UserRepository;
+import gaji.service.domain.user.web.dto.UserRequestDTO;
+import gaji.service.global.exception.RestApiException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
-public class UserCommandServiceImpl implements UserCommandService {
+@RequiredArgsConstructor
+public class UserCommandServiceImpl implements UserCommandService{
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public User cancleUser(Long userId) {
+        User user = updateUserStatus(userId, UserActive.IN_ACTIVE);
+
+        return user;
+    }
+
+    public User updateUserStatus(Long userId, UserActive status) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(()-> new RestApiException(UserErrorStatus._USER_NOT_FOUND));
+
+        user.updateStatus(status);
+
+        return user;
+    }
+
+    @Override
+    @Transactional
+    public User updateUserNickname(Long userId, UserRequestDTO.UpdateNicknameDTO request) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(()-> new RestApiException(UserErrorStatus._USER_NOT_FOUND));
+
+        String newNickname = request.getNickname();
+
+        if (user.getNickname().equals(newNickname)) {
+            throw new RestApiException(UserErrorStatus._NICKNAME_IS_SAME_);
+        }
+        user.updateNickname(newNickname);
+
+        return user;
+    }
+
+    @Override
+    @Transactional
+    public void hardDeleteInactiveUsers() {
+        LocalDateTime cutoffDate = LocalDateTime.now()
+                .minusDays(UserDeletePeriod.ONE_MONTH.getDays());
+        userRepository.deleteAllByInactiveTimeBefore(cutoffDate);
+    }
 }
