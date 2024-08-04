ec48d68b207229cde015f842b5306213f23ca0c1
karryred <ujin037@gmail.com>
2024-08-04 17:03:59 +0900

    :sparkles: Feat: UserRequestDTO, UserRequestDTO 구현

--- src/main/java/gaji/service/domain/user/web/dto/UserResponseDTO.java
+++ src/main/java/gaji/service/domain/user/web/dto/UserResponseDTO.java
@@ -1,13 +1,42 @@
 package gaji.service.domain.user.web.dto;
 
+import gaji.service.domain.enums.UserActive;
+import gaji.service.domain.post.entity.Post;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 public class UserResponseDTO {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CancleResultDTO {
+        Long userId;
+        UserActive userActive;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class GetPostsResultDTO {
+        Long userId;
+        List<Long> postIds;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class UpdateNicknameResultDTO {
+        Long userId;
+        String nickname;
+    }
 }
 
 
