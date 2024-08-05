c6ba0d5bdc90cc228209e20afe0bc9f644f4f9d8
strongmhk <kim6562166086@gmail.com>
2024-08-05 20:48:47 +0900

    :sparkles: [#46] feature: SelectHashtagList 조회 로직 구현

--- src/main/java/gaji/service/domain/common/service/HashtagQueryServiceImpl.java
+++ src/main/java/gaji/service/domain/common/service/HashtagQueryServiceImpl.java
@@ -1,17 +1,31 @@
 package gaji.service.domain.common.service;
 
+import gaji.service.domain.common.converter.HashtagConverter;
+import gaji.service.domain.common.entity.SelectHashtag;
 import gaji.service.domain.common.repository.HashtagRepository;
+import gaji.service.domain.common.repository.SelectHashtagRepository;
+import gaji.service.domain.enums.PostTypeEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class HashtagQueryServiceImpl implements HashtagQueryService {
 
     private final HashtagRepository hashtagRepository;
+    private final SelectHashtagRepository selectHashtagRepository;
 
     @Override
     public boolean ExistHashtagByName(String name) {
         return hashtagRepository.existsByName(name);
     }
+
+    @Override
+    public List<SelectHashtag> findAllFetchJoinWithCategoryByEntityIdAndPostType(Long entityId, PostTypeEnum postType) {
+        return selectHashtagRepository.findAllFetchJoinWithCategoryByEntityIdAndPostType(entityId, postType);
+    }
+
+    // TODO: postId에 해당하는 hashtagList 반환해주는 메서드 만들고 HashtagConverter에 DTO로 변환하는 메서드 만들기
 }
