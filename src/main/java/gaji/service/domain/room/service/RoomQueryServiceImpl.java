771e66f286357bfdd3c53866910eaa252ddfc174
mmingoo <aldrn0323@naver.com>
2024-08-05 15:23:26 +0900

    📝  쿼리 dsl 의존성 추가

--- src/main/java/gaji/service/domain/room/service/RoomQueryServiceImpl.java
+++ src/main/java/gaji/service/domain/room/service/RoomQueryServiceImpl.java
@@ -3,6 +3,8 @@ package gaji.service.domain.room.service;
 import gaji.service.domain.room.code.RoomErrorStatus;
 import gaji.service.domain.room.entity.Room;
 import gaji.service.domain.room.repository.RoomRepository;
+import gaji.service.domain.room.repository.RoomRepositoryCustom;
+import gaji.service.domain.room.web.dto.RoomResponseDto;
 import gaji.service.global.exception.RestApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,13 +13,20 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class RoomQueryServiceImpl implements RoomQueryService {
+public class RoomQueryServiceImpl implements RoomQueryService, RoomRepositoryCustom {
 
+    private final JPAQueryFactory queryFactory;
     private final RoomRepository roomRepository;
 
     @Override
     public Room findRoomById(Long roomId){
         return roomRepository.findById(roomId)
                 .orElseThrow(() -> new RestApiException(RoomErrorStatus._ROOM_NOT_FOUND));
+    }
+
+
+    @Override
+    public RoomResponseDto.StudyRoomInfoDTO getStudyRoomInfo(Long roomId) {
+        return null;
     }
 }
