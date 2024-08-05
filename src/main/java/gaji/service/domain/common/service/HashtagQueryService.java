5935b3afd03a2d7f0f908bfe264dd4b5cfc765fb
strongmhk <kim6562166086@gmail.com>
2024-08-05 20:48:47 +0900

    :sparkles: [#46] feature: SelectHashtagList 조회 로직 구현

--- src/main/java/gaji/service/domain/common/service/HashtagQueryService.java
+++ src/main/java/gaji/service/domain/common/service/HashtagQueryService.java
@@ -1,7 +1,13 @@
 package gaji.service.domain.common.service;
 
 
+import gaji.service.domain.common.entity.SelectHashtag;
+import gaji.service.domain.enums.PostTypeEnum;
+
+import java.util.List;
+
 public interface HashtagQueryService {
 
     boolean ExistHashtagByName(String name);
+    List<SelectHashtag> findAllFetchJoinWithCategoryByEntityIdAndPostType(Long entityId, PostTypeEnum postType);
 }
