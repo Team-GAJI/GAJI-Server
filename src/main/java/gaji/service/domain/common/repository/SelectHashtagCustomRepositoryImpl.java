804c900616787726873b1a1761177fc5b76e7485
strongmhk <kim6562166086@gmail.com>
2024-08-05 20:42:43 +0900

    :sparkles: [#46] feature: entityId와 postType을 통해 category list를 조회하는 Query Dsl Repository 메서드 구현

---
+++ src/main/java/gaji/service/domain/common/repository/SelectHashtagCustomRepositoryImpl.java
@@ -0,0 +1,28 @@
+package gaji.service.domain.common.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import gaji.service.domain.common.entity.SelectHashtag;
+import gaji.service.domain.enums.PostTypeEnum;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static gaji.service.domain.common.entity.QSelectHashtag.selectHashtag;
+import static gaji.service.domain.common.entity.QHashtag.hashtag;
+
+
+@Repository
+@RequiredArgsConstructor
+public class SelectHashtagCustomRepositoryImpl implements SelectHashtagCustomRepository {
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<SelectHashtag> findAllFetchJoinWithCategoryByEntityIdAndPostType(Long entityId, PostTypeEnum postType) {
+        return jpaQueryFactory.selectFrom(selectHashtag)
+                .join(selectHashtag.hashtag, hashtag).fetchJoin()
+                .where(selectHashtag.entityId.eq(entityId), selectHashtag.type.eq(postType))
+                .orderBy(selectHashtag.createdAt.asc())
+                .fetch();
+    }
+}
