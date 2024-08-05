c57cfcdad24811ae93d95d61888b234fbcf7df04
mmingoo <aldrn0323@naver.com>
2024-08-05 18:05:25 +0900

    ✨  RoomMainController 구현

--- src/main/java/gaji/service/domain/room/web/controller/RoomController.java
+++ src/main/java/gaji/service/domain/room/web/controller/RoomController.java
@@ -3,6 +3,7 @@ package gaji.service.domain.room.web.controller;
 import gaji.service.domain.room.converter.RoomConverter;
 import gaji.service.domain.room.entity.RoomNotice;
 import gaji.service.domain.room.service.RoomCommandServiceImpl;
+import gaji.service.domain.room.service.RoomQueryServiceImpl;
 import gaji.service.domain.room.web.dto.RoomRequestDto;
 import gaji.service.domain.room.web.dto.RoomResponseDto;
 import gaji.service.domain.studyMate.Assignment;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class RoomController {
 
     private final RoomCommandServiceImpl roomCommandService;
+    private final RoomQueryServiceImpl queryService;
 
     @PostMapping("/assignments/{roomId}/{userId}")
     @Operation(summary = "스터디룸 과제 등록 API",description = "스터디룸의 과제를 등록하는 API입니다. room의 id가 존재하는지, 스터디에 참혀하고 있는 user인지 검증합니다.")
@@ -33,4 +35,12 @@ public class RoomController {
         RoomNotice roomNotice = roomCommandService.createNotice(roomId, userId, requestDto);
         return BaseResponse.onSuccess(RoomConverter.toRoomNoticeDto(roomNotice));
     }
+
+    @GetMapping("/home/{roomId}")
+    @Operation(summary = "스터디룸 main 화면 스터디 정보 조회 API")
+    public BaseResponse<RoomResponseDto.RoomMainDto> GetRoomMainController(@PathVariable Long roomId){
+        return BaseResponse.onSuccess(queryService.getMainStudyRoom(roomId));
+    }
+
+
 }
