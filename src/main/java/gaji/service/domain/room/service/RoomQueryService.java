7324124bc7179b772d206a593b33abf447b03c38
mmingoo <aldrn0323@naver.com>
2024-08-05 16:59:50 +0900

    ✨  RoomReponseDto 에 MainStudyRoomDTO 생성

--- src/main/java/gaji/service/domain/room/service/RoomQueryService.java
+++ src/main/java/gaji/service/domain/room/service/RoomQueryService.java
@@ -1,6 +1,9 @@
 package gaji.service.domain.room.service;
 
 import gaji.service.domain.room.entity.Room;
+import gaji.service.domain.room.web.dto.RoomResponseDto;
+
+import java.util.List;
 
 public interface RoomQueryService {
 
