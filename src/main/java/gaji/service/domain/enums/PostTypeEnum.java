37e1e04677a28bc27935dba20e0c27aad061037d
strongmhk <kim6562166086@gmail.com>
2024-08-05 20:54:36 +0900

    :sparkles: [#46] feature(PostTypeEnum): 예외처리

--- src/main/java/gaji/service/domain/enums/PostTypeEnum.java
+++ src/main/java/gaji/service/domain/enums/PostTypeEnum.java
@@ -2,11 +2,15 @@ package gaji.service.domain.enums;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
+import gaji.service.domain.post.code.PostErrorStatus;
+import gaji.service.global.exception.RestApiException;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Getter
 @Slf4j
+@RequiredArgsConstructor
 public enum PostTypeEnum {
     BLOG("블로그"),
     PROJECT("프로젝트"),
@@ -14,10 +18,6 @@ public enum PostTypeEnum {
 
     @JsonValue
     private final String value;
-
-    PostTypeEnum(String value) {
-        this.value = value;
-    }
 
     @JsonCreator // Json -> Object, 역직렬화 수행하는 메서드
     public static PostTypeEnum from(String param) {
@@ -27,7 +27,7 @@ public enum PostTypeEnum {
             }
         }
         log.error("PostTypeEnum.from() exception occur param: {}", param);
-        return null;
+        throw new RestApiException(PostErrorStatus._POST_TYPE_NOT_FOUND);
     }
 
 }
