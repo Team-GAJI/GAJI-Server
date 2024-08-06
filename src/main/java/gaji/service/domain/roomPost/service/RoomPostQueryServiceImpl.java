95ac5e0ff4961c9f7fe552f04527db3c36cd0040
mmingoo <aldrn0323@naver.com>
2024-08-06 17:37:00 +0900

    ♻️ RoomPostQueryServiceImpl

--- src/main/java/gaji/service/domain/roomPost/service/RoomPostQueryServiceImpl.java
+++ src/main/java/gaji/service/domain/roomPost/service/RoomPostQueryServiceImpl.java
@@ -2,14 +2,16 @@ package gaji.service.domain.roomPost.service;
 
 import gaji.service.domain.roomPost.repository.RoomPostQueryRepository;
 import gaji.service.domain.roomPost.web.dto.RoomPostResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class RoomPostQueryServiceImpl implements RoomPostQueryService {
 
-    private RoomPostQueryRepository roomPostQueryRepository;
+    private final RoomPostQueryRepository roomPostQueryRepository;
 
     @Override
     public List<RoomPostResponseDto.PostListDto> getTop3RecentPosts(Long roomId) {
