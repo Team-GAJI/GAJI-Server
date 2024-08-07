e277a98e1e674529d0ed253528277b6bf79a2540
karryred <ujin037@gmail.com>
2024-08-07 19:58:58 +0900

    :sparkles: UserResponseDTO 생성

--- src/main/java/gaji/service/domain/user/web/dto/UserResponseDTO.java
+++ src/main/java/gaji/service/domain/user/web/dto/UserResponseDTO.java
@@ -1,13 +1,33 @@
 package gaji.service.domain.user.web.dto;
 
+import gaji.service.domain.enums.PostStatusEnum;
+import gaji.service.domain.enums.PostTypeEnum;
+import gaji.service.domain.user.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
-
 public class UserResponseDTO {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class GetPostDTO {
+        Long postId;
+        Long userId;
+        String nickname;
+        String profileImagePath;
+        String title;
+        String body;
+        PostStatusEnum status;
+        PostTypeEnum type;
+        String createdAt; //DateConverter 이용
+        int viewCnt;
+        int likeCnt;
+
+    }
 }
 
 
