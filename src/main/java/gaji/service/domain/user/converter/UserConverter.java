c58f81e9621e8b41175e6658967a5517abcd286f
karryred <ujin037@gmail.com>
2024-08-07 20:00:06 +0900

    :sparkles: UserConverter 에 GetPostDTO 변환 메서드 추가

--- src/main/java/gaji/service/domain/user/converter/UserConverter.java
+++ src/main/java/gaji/service/domain/user/converter/UserConverter.java
@@ -1,4 +1,23 @@
 package gaji.service.domain.user.converter;
 
+import gaji.service.domain.post.entity.Post;
+import gaji.service.domain.user.entity.User;
+import gaji.service.domain.user.web.dto.UserResponseDTO;
+import gaji.service.global.converter.DateConverter;
+
 public class UserConverter {
+    public static UserResponseDTO.GetPostDTO toGetPostDTO(Post userPost) {
+        User user = userPost.getUser();
+        return UserResponseDTO.GetPostDTO.builder()
+                .postId(userPost.getId())
+                .userId(user.getId())
+                .nickname(user.getNickname())
+                .profileImagePath(user.getProfileImagePth())
+                .title(userPost.getTitle())
+                .body(userPost.getBody())
+                .status(userPost.getStatus())
+                .type(userPost.getType())
+                .createdAt(DateConverter.convertToRelativeTimeFormat(userPost.getCreatedAt()))
+                .build();
+    }
 }
