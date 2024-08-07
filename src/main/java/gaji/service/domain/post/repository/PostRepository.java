5bbdaea65227a496b1847dee952fa547f216c179
karryred <ujin037@gmail.com>
2024-08-07 20:00:34 +0900

    :sparkles: PostRepository에 유저 최신글 찾는 메서드 추가

--- src/main/java/gaji/service/domain/post/repository/PostRepository.java
+++ src/main/java/gaji/service/domain/post/repository/PostRepository.java
@@ -1,7 +1,11 @@
 package gaji.service.domain.post.repository;
 
 import gaji.service.domain.post.entity.Post;
+import gaji.service.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Optional<Post> findFirstByUserOrderByCreatedAtDesc(User user);
 }
