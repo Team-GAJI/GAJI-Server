579c6f832dd5521b3ad43867c7090ad506d74a47
strongmhk <kim6562166086@gmail.com>
2024-08-07 21:52:10 +0900

    :sparkles: [#46] feature: 게시글의 댓글 리스트 조회 비즈니스 로직

--- src/main/java/gaji/service/domain/post/service/CommentService.java
+++ src/main/java/gaji/service/domain/post/service/CommentService.java
@@ -2,8 +2,11 @@ package gaji.service.domain.post.service;
 
 import gaji.service.domain.post.entity.Comment;
 
+import java.util.List;
+
 public interface CommentService {
 
     Comment saveNewComment(Comment comment);
     Comment findByCommentId(Long commentId);
+    List<Comment> findAllByPost(Long postId);
 }
