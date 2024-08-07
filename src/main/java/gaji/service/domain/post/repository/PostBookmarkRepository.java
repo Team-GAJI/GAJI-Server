6e7fde6826bb81c6f1482793d0885d0bf6041aa2
strongmhk <kim6562166086@gmail.com>
2024-08-07 15:27:18 +0900

    :sparkles: [#46] feature(PostBookMarkRepository): 유저가 북마크한 게시물인지 확인하는 메서드

--- src/main/java/gaji/service/domain/post/repository/PostBookmarkRepository.java
+++ src/main/java/gaji/service/domain/post/repository/PostBookmarkRepository.java
@@ -6,6 +6,6 @@ import gaji.service.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostBookmarkRepository extends JpaRepository<PostBookmark, Long> {
-
     void deleteByUserAndPost(User user, Post post);
+    boolean existsByUserIdAndPost(Long userId, Post post);
 }
