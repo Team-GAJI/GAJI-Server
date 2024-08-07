8297ccaa0956bfacad6dcf437e633eaf24d4e515
karryred <ujin037@gmail.com>
2024-08-07 20:26:14 +0900

    :recycle: Refactor: PostRepository에 타입 제한 추가

--- src/main/java/gaji/service/domain/post/repository/PostRepository.java
+++ src/main/java/gaji/service/domain/post/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package gaji.service.domain.post.repository;
 
+import gaji.service.domain.enums.PostTypeEnum;
 import gaji.service.domain.post.entity.Post;
 import gaji.service.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    Optional<Post> findFirstByUserOrderByCreatedAtDesc(User user);
+    Optional<Post> findFirstByUserAndTypeOrderByCreatedAtDesc(User user, PostTypeEnum type);
 }
