1fbc0e260d1426d99bd59185e44d4e799f5f69bf
karryred <ujin037@gmail.com>
2024-08-07 19:58:31 +0900

    :sparkles: UserRestController API 매핑 추가

--- src/main/java/gaji/service/domain/user/web/controller/UserRestController.java
+++ src/main/java/gaji/service/domain/user/web/controller/UserRestController.java
@@ -1,16 +1,17 @@
 package gaji.service.domain.user.web.controller;
 
+import gaji.service.domain.enums.PostTypeEnum;
+import gaji.service.domain.post.entity.Post;
 import gaji.service.domain.user.converter.UserConverter;
 import gaji.service.domain.user.entity.User;
 import gaji.service.domain.user.service.UserCommandService;
 import gaji.service.domain.user.service.UserQueryService;
 import gaji.service.domain.user.web.dto.UserResponseDTO;
 import gaji.service.global.base.BaseResponse;
+import gaji.service.jwt.service.TokenProviderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.token.TokenService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
@@ -18,5 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserRestController {
     private final UserCommandService userCommandService;
     private final UserQueryService userQueryService;
+    private final TokenProviderService tokenProviderService;
+
+    @GetMapping("/")
+    public BaseResponse<UserResponseDTO.GetPostDTO> getMyPost(@RequestHeader("Authorization") String authorizationHeader) {
+        Long userId = tokenProviderService.getUserIdFromToken(authorizationHeader);
+        Post userPost = userQueryService.getUserPost(userId);
+        return BaseResponse.onSuccess(UserConverter.toGetPostDTO(userPost));
+    }
 }
 
