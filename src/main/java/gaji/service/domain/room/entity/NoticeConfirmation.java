97aaa1a2a814543c2c8d9a701594be5ac5ee6815
mmingoo <aldrn0323@naver.com>
2024-08-07 20:11:40 +0900

    ✨   공지사항 확인을 위한 새로운 엔티티 생성

---
+++ src/main/java/gaji/service/domain/room/entity/NoticeConfirmation.java
@@ -0,0 +1,33 @@
+package gaji.service.domain.room.entity;
+
+import gaji.service.domain.studyMate.StudyMate;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class NoticeConfirmation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_notice_id")
+    private RoomNotice roomNotice;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "study_mate_id")
+    private StudyMate studyMate;
+
+    private LocalDateTime confirmedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.confirmedAt = LocalDateTime.now();
+    }
+}
