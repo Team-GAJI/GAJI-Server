e41ee36de8d31b197e1e18df516a72d85761de2f
karryred <ujin037@gmail.com>
2024-08-07 19:57:57 +0900

    :sparkles: UserQueryService 및 Impl 메서드 추가

--- src/main/java/gaji/service/domain/user/service/UserQueryService.java
+++ src/main/java/gaji/service/domain/user/service/UserQueryService.java
@@ -1,11 +1,12 @@
 package gaji.service.domain.user.service;
 
 
+import gaji.service.domain.post.entity.Post;
 import gaji.service.domain.user.entity.User;
 
 public interface UserQueryService {
 
     boolean existUserById(Long userId);
     User findUserById(Long userId);
-
+    Post getUserPost(Long userId);
 }
--- src/main/java/gaji/service/domain/user/service/UserQueryServiceImpl.java
+++ src/main/java/gaji/service/domain/user/service/UserQueryServiceImpl.java
@@ -1,5 +1,9 @@
 package gaji.service.domain.user.service;
 
+import gaji.service.domain.post.code.PostErrorStatus;
+import gaji.service.domain.post.entity.Post;
+import gaji.service.domain.post.repository.PostRepository;
+import gaji.service.domain.post.service.PostCommandService;
 import gaji.service.domain.user.code.UserErrorStatus;
 import gaji.service.domain.user.entity.User;
 import gaji.service.domain.user.repository.UserRepository;
@@ -15,6 +19,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserQueryServiceImpl implements UserQueryService {
 
     private final UserRepository userRepository;
+    private final PostRepository postRepository; // Service 의존 ? Repo의존?
 
     @Override
     public boolean existUserById(Long userId) {
@@ -25,5 +30,15 @@ public class UserQueryServiceImpl implements UserQueryService {
     public User findUserById(Long userId){
         return userRepository.findById(userId)
                 .orElseThrow(() -> new RestApiException(UserErrorStatus._USER_NOT_FOUND));
+    }
+
+    @Override
+    public Post getUserPost(Long userId){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RestApiException(UserErrorStatus._USER_NOT_FOUND));
+        Post post = postRepository.findFirstByUserOrderByCreatedAtDesc(user)
+                .orElseThrow(() -> new RestApiException(UserErrorStatus._POST_NOT_CREATED));
+
+        return post;
     }
 }
