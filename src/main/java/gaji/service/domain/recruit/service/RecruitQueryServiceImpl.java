1a53c67252a1d3a92f667fba888ed763100a2554
= <xzx01299@naver.com>
2024-08-07 20:27:29 +0900

    ♻️ [#43] refactor: 타 도메인의 repository 대신 service를 가져와 사용
    Repository(DAO) 계층은 해당 도메인의 영속성(Entity)를 관리하기 위함이 역할이기에 본인이 아닌 다른 도메인이 해당 영속성을 관리하는 권한을 가지지 않도록 방지

--- src/main/java/gaji/service/domain/recruit/service/RecruitQueryServiceImpl.java
+++ src/main/java/gaji/service/domain/recruit/service/RecruitQueryServiceImpl.java
@@ -4,6 +4,7 @@ import gaji.service.domain.common.entity.SelectCategory;
 import gaji.service.domain.enums.CategoryEnum;
 import gaji.service.domain.enums.PostTypeEnum;
 import gaji.service.domain.recruit.repository.SelectCategoryRepository;
+import gaji.service.domain.room.service.RoomCommandService;
 import gaji.service.domain.user.entity.User;
 import gaji.service.domain.recruit.converter.RecruitConverter;
 import gaji.service.domain.recruit.entity.StudyComment;
@@ -25,8 +26,7 @@ public class RecruitQueryServiceImpl implements RecruitQueryService {
 
     private final UserQueryService userQueryService;
     private final RoomQueryService roomQueryService;
-    private final RoomRepository roomRepository;
-    private final StudyCommentQueryService studyCommentQueryService;
+    private final RoomCommandService roomCommandService;
     private final SelectCategoryRepository selectCategoryRepository;
 
     @Override
@@ -37,7 +37,7 @@ public class RecruitQueryServiceImpl implements RecruitQueryService {
         User user = userQueryService.findUserById(room.getUser().getId());
 
         room.addView();
-        roomRepository.save(room);
+        roomCommandService.saveRoom(room);
 
         List<SelectCategory> selectCategoryList =
                 selectCategoryRepository.findAllByEntityIdAndType(room.getId(), PostTypeEnum.ROOM);
