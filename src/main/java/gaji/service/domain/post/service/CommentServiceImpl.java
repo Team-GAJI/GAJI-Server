87d3f75235acd172a1e3760e14634b98950a7af1
strongmhk <kim6562166086@gmail.com>
2024-08-07 21:52:10 +0900

    :sparkles: [#46] feature: 게시글의 댓글 리스트 조회 비즈니스 로직

--- src/main/java/gaji/service/domain/post/service/CommentServiceImpl.java
+++ src/main/java/gaji/service/domain/post/service/CommentServiceImpl.java
@@ -2,18 +2,21 @@ package gaji.service.domain.post.service;
 
 import gaji.service.domain.post.code.PostErrorStatus;
 import gaji.service.domain.post.entity.Comment;
-import gaji.service.domain.post.repository.CommentRepository;
+import gaji.service.domain.post.entity.Post;
+import gaji.service.domain.post.repository.CommentJpaRepository;
 import gaji.service.global.exception.RestApiException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class CommentServiceImpl implements CommentService {
-
-    private final CommentRepository commentRepository;
+    private final CommentJpaRepository commentRepository;
+    private final PostQueryService postQueryService;
 
     @Override
     @Transactional
@@ -22,8 +25,15 @@ public class CommentServiceImpl implements CommentService {
     }
 
     @Override
+    public List<Comment> findAllByPost(Long postId) {
+        Post findPost = postQueryService.findPostByPostId(postId);
+        return commentRepository.findAllByPostFetchJoinWithUser(findPost);
+    }
+
+    @Override
     public Comment findByCommentId(Long commentId) {
         return commentRepository.findById(commentId)
                 .orElseThrow(() -> new RestApiException(PostErrorStatus._COMMENT_NOT_FOUND));
     }
+
 }
