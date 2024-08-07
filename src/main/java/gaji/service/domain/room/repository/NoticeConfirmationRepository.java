92f77cb99056a4b3e5813811520228e5c1e98c75
mmingoo <aldrn0323@naver.com>
2024-08-07 21:19:31 +0900

    ♻️ confirmation을 위한 로직 수정

--- src/main/java/gaji/service/domain/room/repository/NoticeConfirmationRepository.java
+++ src/main/java/gaji/service/domain/room/repository/NoticeConfirmationRepository.java
@@ -4,5 +4,5 @@ import gaji.service.domain.room.entity.NoticeConfirmation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoticeConfirmationRepository extends JpaRepository<NoticeConfirmation, Long> {
-    boolean existsByRoomNoticeIdAndStudyMateId(Long roomNoticeId, Long studyMateId);
+    NoticeConfirmation findByRoomNoticeIdAndStudyMateId(Long roomNoticeId, Long studyMateId);
 }
