d5d9667959997b7eb5f510cb375df9303a667269
mmingoo <aldrn0323@naver.com>
2024-08-07 20:15:47 +0900

    ✨  confitmation위한 repository 생성

--- src/main/java/gaji/service/domain/room/repository/RoomNoticeRepository.java
+++ src/main/java/gaji/service/domain/room/repository/RoomNoticeRepository.java
@@ -2,8 +2,13 @@ package gaji.service.domain.room.repository;
 
 import gaji.service.domain.room.entity.RoomNotice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoomNoticeRepository extends JpaRepository<RoomNotice, Long> {
+    @Modifying
+    @Query("UPDATE RoomNotice rn SET rn.confirmCount = rn.confirmCount + 1 WHERE rn.id = :noticeId")
+    void incrementConfirmCount(Long noticeId);
 }
