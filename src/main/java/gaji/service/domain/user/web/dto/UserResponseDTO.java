e035ac1e8524ee7e91cfb4d1e5dd42cd3f662c6a
karryred <ujin037@gmail.com>
2024-08-08 15:36:06 +0900

    :sparkles: Feat: UserResponseDTO 에 GetRoomDTO 추가

--- src/main/java/gaji/service/domain/user/web/dto/UserResponseDTO.java
+++ src/main/java/gaji/service/domain/user/web/dto/UserResponseDTO.java
@@ -6,8 +6,30 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class UserResponseDTO {
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class GetRoomDTO {
+        Long roomId;
+        String name;
+        String description;
+        String thumbnail_url;
+        Boolean isOngoing;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class GetRoomListDTO {
+        List<GetRoomDTO> roomList;
+        Integer totalElements;
+    }
 }
 
 
