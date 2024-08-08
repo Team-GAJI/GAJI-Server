b87f6e5acf245fce4a69d063c4492ecbc00eb7fc
karryred <ujin037@gmail.com>
2024-08-08 15:34:08 +0900

    :sparkles: Feat: UserConverter 생성

--- src/main/java/gaji/service/domain/user/converter/UserConverter.java
+++ src/main/java/gaji/service/domain/user/converter/UserConverter.java
@@ -1,4 +1,31 @@
 package gaji.service.domain.user.converter;
 
+import gaji.service.domain.room.entity.Room;
+import gaji.service.domain.user.web.dto.UserResponseDTO;
+import org.springframework.data.domain.Page;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class UserConverter {
+    public static UserResponseDTO.GetRoomDTO toGetRoomDTO(Room room) {
+        return UserResponseDTO.GetRoomDTO.builder()
+                .roomId(room.getId())
+                .name(room.getName())
+                .description(room.getDescription())
+                .thumbnail_url(room.getThumbnailUrl())
+                .isOngoing(room.getStudyEndDay().isAfter(LocalDate.now())) //스터디 종료 날짜가 현재날짜보다 뒤라면 true
+                .build();
+    }
+
+    public static UserResponseDTO.GetRoomListDTO toGetRoomListDTO(List<Room> roomList) {
+        List<UserResponseDTO.GetRoomDTO> GetRoomDTOList = roomList.stream()
+                .map(UserConverter::toGetRoomDTO).toList();
+
+        return UserResponseDTO.GetRoomListDTO.builder()
+                .roomList(GetRoomDTOList)
+                .totalElements(GetRoomDTOList.size())
+                .build();
+    }
 }
