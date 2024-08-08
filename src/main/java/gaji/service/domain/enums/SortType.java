e788e286320f1b4c9553d13455b9244fb381bcc8
spenshark <xzx01299@naver.com>
2024-08-08 13:21:01 +0900

    ✨ [#44] feature: 필터와 정렬을 관리하기 위한 Enum 타입 구현

---
+++ src/main/java/gaji/service/domain/enums/SortType.java
@@ -0,0 +1,32 @@
+package gaji.service.domain.enums;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import gaji.service.global.exception.RestApiException;
+import gaji.service.global.exception.code.status.GlobalErrorStatus;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Getter
+@Slf4j
+@RequiredArgsConstructor
+public enum SortType {
+    RECENT("최신순"),
+    VIEW("조회순"),
+    LIKE("좋아요순");
+
+    @JsonValue
+    private final String value;
+
+    @JsonCreator // Json -> Object, 역직렬화 수행하는 메서드
+    public static SortType from(String param) {
+        for (SortType sortType : SortType.values()) {
+            if (sortType.getValue().equals(param)) {
+                return sortType;
+            }
+        }
+        log.error("SortType.from() exception occur param: {}", param);
+        throw new RestApiException(GlobalErrorStatus._SORT_TYPE_NOT_VALID);
+    }
+}
