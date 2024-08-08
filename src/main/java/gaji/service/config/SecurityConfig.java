9f973a7bfb2719cc17f5007f5ecf1ad0b10c744d
strongmhk <kim6562166086@gmail.com>
2024-08-08 17:36:39 +0900

    :hammer: [#46] fix: 인가 경로 변경

--- src/main/java/gaji/service/config/SecurityConfig.java
+++ src/main/java/gaji/service/config/SecurityConfig.java
@@ -30,12 +30,12 @@ public class SecurityConfig {
     private final CustomSuccessHandler customSuccessHandler;
     private final JWTUtil jwtUtil;
     private final RefreshRepository refreshRepository;
-
-
     private static final String[] AUTH_WHITELIST = {
-            "/oauth2/**", "/swagger-ui/**", "/api-docs", "/swagger-ui-custom.html",
+            "/oauth2/", "/swagger-ui/**", "/swagger-ui-custom.html",
             "/v3/api-docs/**", "/api-docs/**", "/swagger-ui.html", "/reissue", "/", "/my"
+            ,"/api/community-posts/**"
     };
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
