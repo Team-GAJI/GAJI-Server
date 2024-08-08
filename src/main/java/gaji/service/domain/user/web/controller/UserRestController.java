e297376be4294fc68342ffb383ff833a25d689da
karryred <ujin037@gmail.com>
2024-08-08 15:35:25 +0900

    :sparkles: Feat: UserRestController 에 API 경로 추가

--- src/main/java/gaji/service/domain/user/web/controller/UserRestController.java
+++ src/main/java/gaji/service/domain/user/web/controller/UserRestController.java
@@ -1,16 +1,19 @@
 package gaji.service.domain.user.web.controller;
 
+import gaji.service.domain.common.annotation.CheckPage;
+import gaji.service.domain.room.entity.Room;
 import gaji.service.domain.user.converter.UserConverter;
 import gaji.service.domain.user.entity.User;
 import gaji.service.domain.user.service.UserCommandService;
 import gaji.service.domain.user.service.UserQueryService;
 import gaji.service.domain.user.web.dto.UserResponseDTO;
 import gaji.service.global.base.BaseResponse;
+import gaji.service.jwt.service.TokenProviderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -18,5 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserRestController {
     private final UserCommandService userCommandService;
     private final UserQueryService userQueryService;
+    private final TokenProviderService tokenProviderService;
+
+    @GetMapping("/rooms")
+    public BaseResponse<UserResponseDTO.GetRoomListDTO> getMyRoomList(@RequestHeader("Authorization") String authorizationHeader,
+                                                                      @RequestParam(defaultValue = "3") Integer limit) {
+        Long userId = tokenProviderService.getUserIdFromToken(authorizationHeader);
+        List<Room> userRoomList = userQueryService.getUserRoomList(userId, limit);
+        return BaseResponse.onSuccess(UserConverter.toGetRoomListDTO(userRoomList));
+    }
+
+    @GetMapping("/rooms/{userId}")
+    public BaseResponse<UserResponseDTO.GetRoomListDTO> getUserRoomList(@PathVariable Long userId,
+                                                                        @RequestParam(defaultValue = "3") Integer limit) {
+        List<Room> userRoomList = userQueryService.getUserRoomList(userId, limit);
+        return BaseResponse.onSuccess(UserConverter.toGetRoomListDTO(userRoomList));
+    }
 }
 
