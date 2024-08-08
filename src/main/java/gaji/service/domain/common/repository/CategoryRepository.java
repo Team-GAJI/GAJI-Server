f4ade0cf682134f5c92182920d630d66dd5164a3
strongmhk <kim6562166086@gmail.com>
2024-08-08 16:27:18 +0900

    :sparkles: [#46] feature: CategoryRepository 생성

---
+++ src/main/java/gaji/service/domain/common/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package gaji.service.domain.common.repository;
+
+import gaji.service.domain.common.entity.Category;
+import gaji.service.domain.enums.CategoryEnum;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByCategory(CategoryEnum category);
+    boolean existsByCategory(CategoryEnum category);
+}
