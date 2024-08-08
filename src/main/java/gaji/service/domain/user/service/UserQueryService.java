dc28e954b78541e13b4c78360c5371dcffc06a9c
karryred <ujin037@gmail.com>
2024-08-08 15:34:54 +0900

    :sparkles: Feat: UserQueryServiceImpl 에 getUserRoomList 메서드 추가

--- src/main/java/gaji/service/domain/user/service/UserQueryService.java
+++ src/main/java/gaji/service/domain/user/service/UserQueryService.java
@@ -1,11 +1,17 @@
 package gaji.service.domain.user.service;
 
 
+import gaji.service.domain.room.entity.Room;
 import gaji.service.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+
+
+import java.util.List;
 
 public interface UserQueryService {
 
     boolean existUserById(Long userId);
     User findUserById(Long userId);
+    List<Room> getUserRoomList(Long userId, Integer limit);
 
 }
