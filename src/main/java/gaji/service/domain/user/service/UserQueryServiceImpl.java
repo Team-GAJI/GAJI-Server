906a785fd82fd87066fcf6ebab1afaa6b0e9eb12
karryred <ujin037@gmail.com>
2024-08-08 15:34:54 +0900

    :sparkles: Feat: UserQueryServiceImpl 에 getUserRoomList 메서드 추가

--- src/main/java/gaji/service/domain/user/service/UserQueryServiceImpl.java
+++ src/main/java/gaji/service/domain/user/service/UserQueryServiceImpl.java
@@ -1,12 +1,19 @@
 package gaji.service.domain.user.service;
 
+import gaji.service.domain.room.entity.Room;
+import gaji.service.domain.room.repository.RoomCustomRepository;
+import gaji.service.domain.room.repository.RoomRepository;
 import gaji.service.domain.user.code.UserErrorStatus;
 import gaji.service.domain.user.entity.User;
 import gaji.service.domain.user.repository.UserRepository;
 import gaji.service.global.exception.RestApiException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Service
@@ -15,6 +22,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserQueryServiceImpl implements UserQueryService {
 
     private final UserRepository userRepository;
+    private final RoomCustomRepository roomCustomRepository;
 
     @Override
     public boolean existUserById(Long userId) {
@@ -25,5 +33,14 @@ public class UserQueryServiceImpl implements UserQueryService {
     public User findUserById(Long userId){
         return userRepository.findById(userId)
                 .orElseThrow(() -> new RestApiException(UserErrorStatus._USER_NOT_FOUND));
+    }
+
+    @Override
+    public List<Room> getUserRoomList(Long userId, Integer limit) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RestApiException(UserErrorStatus._USER_NOT_FOUND));
+        List<Room> roomList = roomCustomRepository.findTop3OngoingAndTop3EndedRoomsByUser(user, limit);
+
+        return roomList;
     }
 }
