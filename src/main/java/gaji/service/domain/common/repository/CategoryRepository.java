713a5e45dced392d2c8828c0e2e4326ea6f6ba58
strongmhk <kim6562166086@gmail.com>
2024-08-09 16:37:03 +0900

    :sparkles: [#46] feature(CategoryRepository): Category 존재 여부 메서드 구현

--- src/main/java/gaji/service/domain/common/repository/CategoryRepository.java
+++ src/main/java/gaji/service/domain/common/repository/CategoryRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     Category findByCategory(CategoryEnum category);
     boolean existsByCategory(CategoryEnum category);
+    boolean existsById(Long id);
 }
