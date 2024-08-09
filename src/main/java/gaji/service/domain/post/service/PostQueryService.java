0056386208a178c6c8c5e718146d8ba3ba4da323
strongmhk <kim6562166086@gmail.com>
2024-08-09 19:05:58 +0900

    :sparkles: [#46] feature: Post Service 계층에서 Category에 해당하는 Post 조회 로직 추가

--- src/main/java/gaji/service/domain/post/service/PostQueryService.java
+++ src/main/java/gaji/service/domain/post/service/PostQueryService.java
@@ -1,13 +1,11 @@
 package gaji.service.domain.post.service;
 
-import gaji.service.domain.enums.CategoryEnum;
 import gaji.service.domain.enums.SortType;
 import gaji.service.domain.enums.PostStatusEnum;
 import gaji.service.domain.enums.PostTypeEnum;
 import gaji.service.domain.post.entity.Post;
 import org.springframework.data.domain.Slice;
 
-import java.util.List;
 
 public interface PostQueryService {
 
@@ -17,7 +15,7 @@ public interface PostQueryService {
                             Integer lastLikeCnt,
                             Integer lastHit,
                             PostTypeEnum postType,
-                            CategoryEnum category,
+                            Long categoryId,
                             SortType sortType,
                             PostStatusEnum filter,
                             int size);
