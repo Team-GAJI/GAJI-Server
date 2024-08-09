003d0e4c76c6f743542da25fa1dfd0139eca381c
mmingoo <aldrn0323@naver.com>
2024-08-09 20:26:37 +0900

    ♻️ userId를 토큰방식 통해 추출할 수 있게끔 수정

--- src/main/java/gaji/service/domain/room/web/controller/RoomMainController.java
+++ src/main/java/gaji/service/domain/room/web/controller/RoomMainController.java
@@ -9,6 +9,7 @@ import gaji.service.domain.room.web.dto.RoomRequestDto;
 import gaji.service.domain.room.web.dto.RoomResponseDto;
 import gaji.service.domain.studyMate.entity.Assignment;
 import gaji.service.global.base.BaseResponse;
+import gaji.service.jwt.service.TokenProviderService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
@@ -25,43 +26,52 @@ public class RoomMainController {
 
     private final RoomCommandService roomCommandService;
     private final RoomQueryService roomQueryService;
+    private final TokenProviderService tokenProviderService;
     @PostMapping("/assignments/{roomId}/{userId}")
     @Operation(summary = "스터디룸 과제 등록 API",description = "스터디룸의 과제를 등록하는 API입니다. room의 id가 존재하는지, 스터디에 참혀하고 있는 user인지 검증합니다.")
-    public BaseResponse<Long> AssignmentController(@PathVariable Long userId/*하드 코딩용, 추후 삭제*/, @RequestBody @Valid RoomRequestDto.AssignmentDto requestDto, @PathVariable Long roomId){
-        //Long userId = getUserIdFromToken(token);
+    public BaseResponse<Long> AssignmentController(
+            @RequestBody @Valid RoomRequestDto.AssignmentDto requestDto,
+            @PathVariable Long roomId,
+            @RequestHeader("Authorization") String authorizationHeader){
+
+        Long userId = tokenProviderService.getUserIdFromToken(authorizationHeader);
         Assignment assignment = roomCommandService.createAssignment(roomId, userId, requestDto);
         return BaseResponse.onSuccess(assignment.getId());
     }
 
-    @PostMapping("/event/{roomId}/{weeks}/{userId}/period")
+    @PostMapping("/event/{roomId}/{weeks}/period")
     @Operation(summary = "스터디룸 기간 설정 API", description = "스터디룸의 전체 기간을 설정하는 API입니다.")
     public BaseResponse<Long> setStudyPeriod(
             @PathVariable Integer weeks,
-            @PathVariable Long userId,
             @PathVariable Long roomId,
-            @RequestBody @Valid RoomRequestDto.StudyPeriodDto requestDto
-    ) {
+            @RequestHeader("Authorization") String authorizationHeader,
+            @RequestBody @Valid RoomRequestDto.StudyPeriodDto requestDto) {
+
+        Long userId = tokenProviderService.getUserIdFromToken(authorizationHeader);
         RoomEvent event = roomCommandService.setStudyPeriod(roomId,weeks, userId, requestDto);
         return BaseResponse.onSuccess(event.getId());
     }
 
-    @PostMapping("/event/{roomId}/{weeks}/{userId}/description")
+    @PostMapping("/event/{roomId}/{weeks}/description")
     @Operation(summary = "스터디룸 설명 입력 API", description = "스터디룸에 대한 설명을 입력하는 API입니다.")
     public BaseResponse<Long> setStudyDescription(
             @PathVariable @Min(value = 1, message = "Weeks must be at least 1") Integer weeks,
-            @PathVariable Long userId,
             @PathVariable Long roomId,
-            @RequestBody @Valid RoomRequestDto.StudyDescriptionDto requestDto
-    ) {
+            @RequestHeader("Authorization") String authorizationHeader,
+            @RequestBody @Valid RoomRequestDto.StudyDescriptionDto requestDto) {
+        Long userId = tokenProviderService.getUserIdFromToken(authorizationHeader);
         RoomEvent event = roomCommandService.setStudyDescription(roomId, weeks, userId, requestDto);
+
         return BaseResponse.onSuccess(event.getId());
     }
 
-    @PostMapping("/notice/{userAssignmentId}/{userId}")
+    @PostMapping("/notice/{userAssignmentId}")
     @Operation(summary = "주차별 과제 체크 박스 체크", description = "과제 체크 박스를 클릭하면 과제 완료 .")
     public ResponseEntity<RoomResponseDto.AssignmentProgressResponse> toggleAssignmentCompletion(
-            @PathVariable Long userId,
+            @RequestHeader("Authorization") String authorizationHeader,
             @PathVariable Long userAssignmentId) {
+
+        Long userId = tokenProviderService.getUserIdFromToken(authorizationHeader);
         RoomResponseDto.AssignmentProgressResponse response = roomCommandService.toggleAssignmentCompletion(userId, userAssignmentId);
         return ResponseEntity.ok(response);
     }
