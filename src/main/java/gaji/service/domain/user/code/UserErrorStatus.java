5b70c544a54b1624bda24ec58bf24e08a17e85e9
karryred <ujin037@gmail.com>
2024-08-09 11:46:11 +0900

    :sparkles: Feat: _USER_IS_NOT_SAME_ 에러 추가

--- src/main/java/gaji/service/domain/user/code/UserErrorStatus.java
+++ src/main/java/gaji/service/domain/user/code/UserErrorStatus.java
@@ -11,7 +11,10 @@ import org.springframework.http.HttpStatus;
 public enum UserErrorStatus implements BaseErrorCodeInterface {
 
     _USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "USER_4001", "존재하지 않는 회원입니다."),
+
+    //nickname 관련 Error
     _NICKNAME_IS_SAME_(HttpStatus.BAD_REQUEST, "USER_4002", "전과 동일한 닉네임으로 수정할 수 없습니다."),
+    _USER_IS_NOT_SAME_(HttpStatus.BAD_REQUEST, "USER_4003", "다른 회원의 닉네임을 수정할 수 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
