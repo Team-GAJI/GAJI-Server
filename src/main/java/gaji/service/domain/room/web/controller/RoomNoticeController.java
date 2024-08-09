d553b4639ca3b3a74b6481c414e94b6cbd24a54e
mmingoo <aldrn0323@naver.com>
2024-08-09 20:26:37 +0900

    ♻️ userId를 토큰방식 통해 추출할 수 있게끔 수정

--- src/main/java/gaji/service/domain/room/web/controller/RoomNoticeController.java
+++ src/main/java/gaji/service/domain/room/web/controller/RoomNoticeController.java
@@ -7,6 +7,7 @@ import gaji.service.domain.room.service.RoomQueryService;
 import gaji.service.domain.room.web.dto.RoomRequestDto;
 import gaji.service.domain.room.web.dto.RoomResponseDto;
 import gaji.service.global.base.BaseResponse;
+import gaji.service.jwt.service.TokenProviderService;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -22,12 +23,19 @@ public class RoomNoticeController {
 
     private final RoomCommandService roomCommandService;
     private final RoomQueryService roomQueryService;
+    private final TokenProviderService tokenProviderService;
 
 
-    @PostMapping("/notices/{roomId}/{userId}")
+    @PostMapping("/notices/{roomId}")
     @Operation(summary = "스터디룸 공지 등록 API",description = "스터디룸의 공지를 등록하는 API입니다. room의 id가 존재하는지, 작성자가 Reader인지 검증합니다.")
-    public BaseResponse<RoomResponseDto.RoomNoticeDto> NoticeController(@PathVariable Long userId/*하드 코딩용, 추후 삭제*/, @RequestBody @Valid RoomRequestDto.RoomNoticeDto requestDto, @PathVariable Long roomId) {
+    public BaseResponse<RoomResponseDto.RoomNoticeDto> NoticeController(
+            @RequestBody @Valid RoomRequestDto.RoomNoticeDto requestDto,
+            @RequestHeader("Authorization") String authorizationHeader,
+            @PathVariable Long roomId) {
+
+        Long userId = tokenProviderService.getUserIdFromToken(authorizationHeader);
         RoomNotice roomNotice = roomCommandService.createNotice(roomId, userId, requestDto);
+
         return BaseResponse.onSuccess(RoomConverter.toRoomNoticeDto(roomNotice));
     }
 
@@ -53,8 +61,13 @@ public class RoomNoticeController {
 
     @PostMapping("/notice/{noticeId}/confirm/{userId}")
     @Operation(summary = "스터디룸 공지 확인 버튼 누르기 API", description = "공지사항 확인 상태를 토글합니다.")
-    public ResponseEntity<Boolean> toggleNoticeConfirmation(@PathVariable Long noticeId, @PathVariable Long userId) {
+    public ResponseEntity<Boolean> toggleNoticeConfirmation(
+            @PathVariable Long noticeId,
+            @RequestHeader("Authorization") String authorizationHeader) {
+
+        Long userId = tokenProviderService.getUserIdFromToken(authorizationHeader);
         boolean isConfirmed = roomCommandService.toggleNoticeConfirmation(noticeId,userId);
+
         return ResponseEntity.ok(isConfirmed);
     }
 
--- src/main/java/gaji/service/domain/roomPost/web/controller/RoomPostController.java
+++ src/main/java/gaji/service/domain/roomPost/web/controller/RoomPostController.java
@@ -7,6 +7,7 @@ import gaji.service.domain.roomPost.service.RoomPostQueryService;
 import gaji.service.domain.roomPost.web.dto.RoomPostRequestDto;
 import gaji.service.domain.roomPost.web.dto.RoomPostResponseDto;
 import gaji.service.global.base.BaseResponse;
+import gaji.service.jwt.service.TokenProviderService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -18,11 +19,15 @@ import java.util.List;
 @RequestMapping("/api/studyRooms")
 public class RoomPostController {
     private final RoomPostQueryService roomPostQueryService;
-
+    private TokenProviderService tokenProviderService;
 
     @GetMapping("/api/studyRooms/post/{roomId}")
     @Operation(summary = "스터디룸 main 화면 게시글 조회 API")
-    public BaseResponse<List<RoomPostResponseDto.PostListDto>> MainRoomPostController(@PathVariable Long roomId){
+    public BaseResponse<List<RoomPostResponseDto.PostListDto>> MainRoomPostController(
+            @PathVariable Long roomId,
+            @RequestHeader("Authorization") String authorizationHeader){
+
+        Long userId = tokenProviderService.getUserIdFromToken(authorizationHeader);
         return BaseResponse.onSuccess(roomPostQueryService.getTop3RecentPosts(roomId));
 
     }
