ac788c7dfe5ff06025e5e6c7c8e7218c9d90b91b
strongmhk <kim6562166086@gmail.com>
2024-08-09 18:57:08 +0900

    :poop: [#46] need_refactor: 검증 메시지 적용 안되는 문제 해결 필요

--- src/main/java/gaji/service/domain/post/validation/PostTypeExistValidator.java
+++ src/main/java/gaji/service/domain/post/validation/PostTypeExistValidator.java
@@ -7,6 +7,8 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Component
 public class PostTypeExistValidator implements ConstraintValidator<ExistPostType, PostTypeEnum> {
 
@@ -15,9 +17,20 @@ public class PostTypeExistValidator implements ConstraintValidator<ExistPostType
         ConstraintValidator.super.initialize(constraintAnnotation);
     }
 
+    // TODO: 검증 메시지 적용 안되는 문제 해결하기
     @Override
     public boolean isValid(PostTypeEnum value, ConstraintValidatorContext context) {
-        boolean isValid = value == PostTypeEnum.BLOG || value == PostTypeEnum.PROJECT || value == PostTypeEnum.QUESTION;
+//        boolean isValid = (value == PostTypeEnum.ROOM) || (value == PostTypeEnum.BLOG) || (value == PostTypeEnum.PROJECT) || (value == PostTypeEnum.QUESTION);
+//        boolean isValid = false;
+//
+//        for (PostTypeEnum postTypeEnum : PostTypeEnum.values()) {
+//            if (postTypeEnum.equals(value)) {
+//                isValid = true;
+//            }
+//        }
+
+        boolean isValid = Arrays.stream(PostTypeEnum.values())
+                .allMatch(postTypeEnum -> postTypeEnum.equals(value));
 
         if (!isValid) {
             context.disableDefaultConstraintViolation();
