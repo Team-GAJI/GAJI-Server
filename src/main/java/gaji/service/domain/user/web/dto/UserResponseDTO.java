d150f4ff5fb3f912535f0629032a403af8badfdc
karryred <ujin037@gmail.com>
2024-08-10 18:58:01 +0900

    :recycle: Refactor: UserResponseDTO 반환값 수정

--- src/main/java/gaji/service/domain/user/web/dto/UserResponseDTO.java
+++ src/main/java/gaji/service/domain/user/web/dto/UserResponseDTO.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -19,7 +20,7 @@ public class UserResponseDTO {
         String name;
         String description;
         String thumbnail_url;
-        Boolean isOngoing;
+        LocalDate studyStartDay;
     }
 
     @Builder
