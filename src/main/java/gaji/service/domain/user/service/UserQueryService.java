72ef5a31891aaf4ed47b2ae8e1c5dd7d1c38b233
karryred <ujin037@gmail.com>
2024-08-10 18:57:00 +0900

    :recycle: Refactor: UserQueryService 및 Impl getUserRoomList 메서드 수정

--- src/main/java/gaji/service/domain/user/service/UserQueryService.java
+++ src/main/java/gaji/service/domain/user/service/UserQueryService.java
@@ -1,15 +1,17 @@
 package gaji.service.domain.user.service;
 
 
-import gaji.service.domain.room.entity.Room;
+import com.querydsl.core.Tuple;
+import gaji.service.domain.enums.RoomTypeEnum;
 import gaji.service.domain.user.entity.User;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface UserQueryService {
 
     boolean existUserById(Long userId);
     User findUserById(Long userId);
-    List<Room> getUserRoomList(Long userId, Integer limit);
+    List<Tuple> getUserRoomList(Long userId, LocalDate cursorDate, Long cursorId, RoomTypeEnum type, Integer limit);
 
 }
