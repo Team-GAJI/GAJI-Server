28542fa4dcb81645a34fc7dacfc290d875e5315a
shimfff <shimseawon1510@gmail.com>
2024-08-11 23:23:56 +0900

    :sparkles: Feat: User를 찾기 위한 findById 메서드 추가

--- src/main/java/gaji/service/domain/user/service/UserCommandServiceImpl.java
+++ src/main/java/gaji/service/domain/user/service/UserCommandServiceImpl.java
@@ -1,7 +1,20 @@
 package gaji.service.domain.user.service;
 
+import gaji.service.domain.user.code.UserErrorStatus;
+import gaji.service.domain.user.entity.User;
+import gaji.service.domain.user.repository.UserRepository;
+import gaji.service.global.exception.RestApiException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserCommandServiceImpl implements UserCommandService {
+
+    UserRepository userRepository;
+
+    @Override
+    public User findById(Long userId){
+        return userRepository.findById(userId).orElseThrow(
+                () -> new RestApiException(UserErrorStatus._USER_NOT_FOUND)
+        );
+    }
 }
