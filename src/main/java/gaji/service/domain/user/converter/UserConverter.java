1b555a06dd6214a69011cb1fcbacf88c924e5c59
karryred <ujin037@gmail.com>
2024-08-12 21:56:41 +0900

    :sparkles: Feat: UserConverter 생성

--- src/main/java/gaji/service/domain/user/converter/UserConverter.java
+++ src/main/java/gaji/service/domain/user/converter/UserConverter.java
@@ -1,4 +1,14 @@
 package gaji.service.domain.user.converter;
 
+import gaji.service.domain.user.entity.User;
+import gaji.service.domain.user.web.dto.UserResponseDTO;
+
 public class UserConverter {
+    public static UserResponseDTO.GetUserDetailDTO toGetUserDetailDTO(User user) {
+        return UserResponseDTO.GetUserDetailDTO.builder()
+                .userId(user.getId())
+                .nickname(user.getNickname())
+                .profileImagePth(user.getProfileImagePth())
+                .build();
+    }
 }
