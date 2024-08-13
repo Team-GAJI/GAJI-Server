3435916c83df501f96312f0c1e8a27ad45a199ac
mmingoo <aldrn0323@naver.com>
2024-08-13 22:49:04 +0900

    ✨  정보 나눔 게시글 업로드

--- src/main/java/gaji/service/domain/roomBoard/converter/RoomPostConverter.java
+++ src/main/java/gaji/service/domain/roomBoard/converter/RoomPostConverter.java
@@ -1,5 +1,6 @@
 package gaji.service.domain.roomBoard.converter;
 
+import gaji.service.domain.roomBoard.entity.RoomInfoPost;
 import gaji.service.domain.roomBoard.entity.common.RoomBoard;
 import gaji.service.domain.roomBoard.entity.RoomPost;
 import gaji.service.domain.roomBoard.entity.RoomTroublePost;
@@ -35,6 +36,21 @@ public class RoomPostConverter {
     public static RoomPostResponseDto.toCreateRoomTroublePostIdDTO troublePostIdDto(Long id){
         return RoomPostResponseDto.toCreateRoomTroublePostIdDTO.builder()
                 .troublePostId(id)
+                .build();
+    }
+
+    public static RoomInfoPost toRoomInfoPost(RoomPostRequestDto.RoomInfoPostDto requestDto, StudyMate studyMate, RoomBoard roomBoard) {
+        return RoomInfoPost.builder()
+                .studyMate(studyMate)
+                .title(requestDto.getTitle())
+                .body(requestDto.getBody())
+                .postTime(LocalDateTime.now())
+                .roomBoard(roomBoard)
+                .build();
+    }
+    public static RoomPostResponseDto.toCreateRoomInfoPostIdDTO infoPostIdDto(Long id){
+        return RoomPostResponseDto.toCreateRoomInfoPostIdDTO.builder()
+                .infoPostId(id)
                 .build();
     }
 
