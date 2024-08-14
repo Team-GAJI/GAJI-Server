3421d022827184f3326ba0c8e331ea7f2f6a9334
mmingoo <aldrn0323@naver.com>
2024-08-14 19:16:38 +0900

    ✨   게시글 좋아요 누르기

--- src/main/java/gaji/service/domain/roomBoard/service/RoomTroublePostCommandService.java
+++ src/main/java/gaji/service/domain/roomBoard/service/RoomTroublePostCommandService.java
@@ -1,9 +1,11 @@
 package gaji.service.domain.roomBoard.service;
 
+import gaji.service.domain.enums.PostLikeStatus;
 import gaji.service.domain.roomBoard.entity.RoomTroublePost;
 import gaji.service.domain.roomBoard.entity.TroublePostComment;
 import gaji.service.domain.roomBoard.web.dto.RoomPostRequestDto;
 import gaji.service.domain.roomBoard.web.dto.RoomPostResponseDto;
+import jakarta.transaction.Transactional;
 
 public interface RoomTroublePostCommandService {
     TroublePostComment writeCommentOnTroublePost(Long userId, Long postId, RoomPostRequestDto.RoomTroubleCommentDto request);
@@ -11,4 +13,7 @@ public interface RoomTroublePostCommandService {
     RoomPostResponseDto.toCreateRoomTroublePostIdDTO createRoomTroublePost(Long roomId, Long userId, RoomPostRequestDto.RoomTroubloePostDto requestDto);
 
     RoomTroublePost findTroublePostById(Long postId);
+
+    @Transactional
+    PostLikeStatus toggleLike(Long postId, Long studyMateId);
 }
