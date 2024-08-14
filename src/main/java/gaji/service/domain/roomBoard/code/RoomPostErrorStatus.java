ae3a6b7e627709bf19aa184ebd6dca8fdbf777b2
mmingoo <aldrn0323@naver.com>
2024-08-14 21:01:19 +0900

    ✨  댓글 삭제 기능 구현

--- src/main/java/gaji/service/domain/roomBoard/code/RoomPostErrorStatus.java
+++ src/main/java/gaji/service/domain/roomBoard/code/RoomPostErrorStatus.java
@@ -17,7 +17,8 @@ public enum RoomPostErrorStatus implements BaseErrorCodeInterface {
 
     _USER_NOT_DELETE_AUTH(HttpStatus.BAD_REQUEST, "AUTH_4001", "게시글을 삭제할 권한이 없습니다."),
     _USER_NOT_UPDATE_AUTH(HttpStatus.BAD_REQUEST, "AUTH_4002", "게시글을 수정할 권한이 없습니다."),
-    _USER_NOT_COMMENT_UPDATE_AUTH(HttpStatus.BAD_REQUEST, "AUTH_4002", "댓글을 수정할 권한이 없습니다."),
+    _USER_NOT_COMMENT_UPDATE_AUTH(HttpStatus.BAD_REQUEST, "AUTH_4003", "댓글을 수정할 권한이 없습니다."),
+    _USER_NOT_COMMENT_DELETE_AUTH(HttpStatus.BAD_REQUEST, "AUTH_4004", "댓글을 삭제할 권한이 없습니다."),
 
     _NOT_FOUND_COMMENT(HttpStatus.BAD_REQUEST, "COMMENT_4001", "존재하지 않는 댓글입니다.");
 
