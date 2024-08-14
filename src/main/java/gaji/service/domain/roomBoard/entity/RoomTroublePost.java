6cd447ee9cf19701ddcb958b37620adb15050327
mmingoo <aldrn0323@naver.com>
2024-08-14 18:11:40 +0900

    ✨   RoomTroublePost 와 좋아요 테이블 의존성 추가

--- src/main/java/gaji/service/domain/roomBoard/entity/RoomTroublePost.java
+++ src/main/java/gaji/service/domain/roomBoard/entity/RoomTroublePost.java
@@ -17,7 +17,6 @@ public class RoomTroublePost {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String title;
     private String body;
     private int viewCount;
@@ -29,18 +28,30 @@ public class RoomTroublePost {
     @JoinColumn(name = "board_id")
     private RoomBoard roomBoard;
 
-    @OneToMany(mappedBy = "roomTroublePost", cascade =  CascadeType.ALL)
+    @OneToMany(mappedBy = "roomTroublePost", cascade = CascadeType.ALL)
     private List<TroublePostComment> troublePostCommentList = new ArrayList<>();
-
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "study_mate_id")
     private StudyMate studyMate;
+
+    @OneToMany(mappedBy = "roomTroublePost", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<RoomTroublePostLike> likes = new ArrayList<>();
 
     @PrePersist
     public void prePersist() {
         this.viewCount = 0;
         this.likeCount = 0;
         this.bookmarkCount = 0;
+    }
+
+    public void addLike(RoomTroublePostLike like) {
+        this.likes.add(like);
+        this.likeCount++;
+    }
+
+    public void removeLike(RoomTroublePostLike like) {
+        this.likes.remove(like);
+        this.likeCount = Math.max(0, this.likeCount - 1);
     }
 }
