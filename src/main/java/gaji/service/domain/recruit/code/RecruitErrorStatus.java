572d681e7691b8ccc16b95457a12a41b6d515374
spenshark <xzx01299@naver.com>
2024-08-15 04:54:48 +0900

    ✨ [#86] feature(RecruitErrorStatus): 댓글이 없을 때의 에러 코드 추가

--- src/main/java/gaji/service/domain/recruit/code/RecruitErrorStatus.java
+++ src/main/java/gaji/service/domain/recruit/code/RecruitErrorStatus.java
@@ -13,6 +13,8 @@ public enum RecruitErrorStatus implements BaseErrorCodeInterface {
     _USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "USER_4001","사용자를 찾을 수 없습니다."), // 임시 생성
     _RECRUIT_POST_NOT_FOUND(HttpStatus.BAD_REQUEST, "RECRUIT_4001", "모집 게시글을 찾을 수 없습니다."),
     _RECRUIT_CATEGORY_NOT_FOUND(HttpStatus.BAD_REQUEST, "RECRUIT_4002", "해당 카테고리가 존재하지 않습니다."),
+
+    _COMMENT_NOT_FOUND(HttpStatus.BAD_REQUEST, "RECRUIT_4003", "존재하지 않는 댓글입니다."),
     ;
 
     private final HttpStatus httpStatus;
