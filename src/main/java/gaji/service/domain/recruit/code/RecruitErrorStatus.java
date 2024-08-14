8ad3053ae64269b0492bfa4c0dc6423500457f81
spenshark <xzx01299@naver.com>
2024-08-15 02:06:26 +0900

    ✨ [#85] feature(RecruitErrorStatus): 이미 존재하는 좋아요와 북마크 관련 에러 추가

--- src/main/java/gaji/service/domain/recruit/code/RecruitErrorStatus.java
+++ src/main/java/gaji/service/domain/recruit/code/RecruitErrorStatus.java
@@ -13,6 +13,9 @@ public enum RecruitErrorStatus implements BaseErrorCodeInterface {
     _USER_NOT_FOUND(HttpStatus.BAD_REQUEST, "USER_4001","사용자를 찾을 수 없습니다."), // 임시 생성
     _RECRUIT_POST_NOT_FOUND(HttpStatus.BAD_REQUEST, "RECRUIT_4001", "모집 게시글을 찾을 수 없습니다."),
     _RECRUIT_CATEGORY_NOT_FOUND(HttpStatus.BAD_REQUEST, "RECRUIT_4002", "해당 카테고리가 존재하지 않습니다."),
+
+    _ROOM_ALREADY_LIKE(HttpStatus.BAD_REQUEST, "LIKE_4001", "이미 좋아요 된 게시글 입니다."),
+    _ROOM_ALREADY_BOOKMARK(HttpStatus.BAD_REQUEST, "BOOKMARK_4001", "이미 북마크 된 게시글 입니다."),
     ;
 
     private final HttpStatus httpStatus;
