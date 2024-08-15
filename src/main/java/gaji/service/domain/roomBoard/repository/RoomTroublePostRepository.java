6dc094bb4d622fd24ec000cd53d20e5fb0c4b69c
mmingoo <aldrn0323@naver.com>
2024-08-15 10:58:26 +0900

    ✨  게시글 조회를 위한 쿼리 작성
    jpql 방식 사용

--- src/main/java/gaji/service/domain/roomBoard/repository/RoomTroublePostRepository.java
+++ src/main/java/gaji/service/domain/roomBoard/repository/RoomTroublePostRepository.java
@@ -2,8 +2,19 @@ package gaji.service.domain.roomBoard.repository;
 
 import gaji.service.domain.roomBoard.entity.RoomTroublePost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.awt.print.Pageable;
 
 @Repository
 public interface RoomTroublePostRepository extends JpaRepository<RoomTroublePost, Long> {
+    @Query("SELECT new gaji.service.domain.roomBoard.dto.TroublePostSummaryDto(" +
+            "r.id, r.title, r.studyMate.user.id, r.createdAt, r.viewCount, SIZE(r.troublePostCommentList)) " +
+            "FROM RoomTroublePost r " +
+            "WHERE r.roomBoard.id = :boardId " +
+            "ORDER BY r.createdAt DESC")
+    List<TroublePostSummaryDto> findTroublePostSummaries(@Param("boardId") Long boardId, Pageable pageable);
+
 }
