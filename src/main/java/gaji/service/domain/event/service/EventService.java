5fffe4ce7aaf389eb9eba6dba8ff2b1078079753
shimfff <shimseawon1510@gmail.com>
2024-08-15 16:05:49 +0900

    :recycle: Refactor: public 삭제

--- src/main/java/gaji/service/domain/event/service/EventService.java
+++ src/main/java/gaji/service/domain/event/service/EventService.java
@@ -8,10 +8,10 @@ import java.time.LocalDateTime;
 
 public interface EventService {
 
-    public EventInfoListResponse getEventList(LocalDate date, Long userId);
-    public Long putEvent(LocalDateTime date, Long userId, Long myId, EventInfoRequest request);
-    public Long patchEvent(Long eventId, Long myId, EventInfoRequest request);
-    public Long deleteEvent(Long eventId, Long myId);
-    public Long putEventComplete(Long eventId, Long myId);
-    public Long deleteEventComplete(Long eventId, Long myId);
+    EventInfoListResponse getEventList(LocalDate date, Long userId);
+    Long putEvent(LocalDateTime date, Long userId, Long myId, EventInfoRequest request);
+    Long patchEvent(Long eventId, Long myId, EventInfoRequest request);
+    Long deleteEvent(Long eventId, Long myId);
+    Long putEventComplete(Long eventId, Long myId);
+    Long deleteEventComplete(Long eventId, Long myId);
 }
