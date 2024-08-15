4da22230f0771b2a5e3b3686d37d0c5cd8876711
mmingoo <aldrn0323@naver.com>
2024-08-15 11:42:37 +0900

    ♻️ 조회 repository dto 경로 수정

--- src/main/java/gaji/service/domain/roomBoard/repository/RoomTroublePostRepository.java
+++ src/main/java/gaji/service/domain/roomBoard/repository/RoomTroublePostRepository.java
@@ -2,6 +2,7 @@ package gaji.service.domain.roomBoard.repository;
 
 import gaji.service.domain.roomBoard.entity.RoomTroublePost;
 import gaji.service.domain.roomBoard.web.dto.RoomPostResponseDto;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,11 +13,11 @@ import java.util.List;
 
 @Repository
 public interface RoomTroublePostRepository extends JpaRepository<RoomTroublePost, Long> {
-    @Query("SELECT new gaji.service.domain.roomBoard.dto.TroublePostSummaryDto(" +
+    @Query("SELECT new gaji.service.domain.roomBoard.web.dto.RoomPostResponseDto$TroublePostSummaryDto(" +
             "r.id, r.title, r.studyMate.user.id, r.createdAt, r.viewCount, SIZE(r.troublePostCommentList)) " +
             "FROM RoomTroublePost r " +
             "WHERE r.roomBoard.id = :boardId " +
             "ORDER BY r.createdAt DESC")
-    List<RoomPostResponseDto.TroublePostSummaryDto> findTroublePostSummaries(@Param("boardId") Long boardId, Pageable pageable);
+    List<RoomPostResponseDto.TroublePostSummaryDto> findTroublePostSummaries(@Param("boardId") Long boardId, PageRequest pageRequest);
 
 }
