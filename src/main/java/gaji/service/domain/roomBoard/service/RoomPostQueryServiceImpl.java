4a901628dc56491c6e192eb3204a5b2ebcbc3541
mmingoo <aldrn0323@naver.com>
2024-08-15 11:04:53 +0900

    ✨  게시글 조회를 위한 getPaginatedTroublePosts 생성

--- src/main/java/gaji/service/domain/roomBoard/service/RoomPostQueryServiceImpl.java
+++ src/main/java/gaji/service/domain/roomBoard/service/RoomPostQueryServiceImpl.java
@@ -1,10 +1,13 @@
 package gaji.service.domain.roomBoard.service;
 
 import gaji.service.domain.roomBoard.repository.RoomPostQueryRepository;
+import gaji.service.domain.roomBoard.repository.RoomTroublePostRepository;
 import gaji.service.domain.roomBoard.web.dto.RoomPostResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 @Service
@@ -12,10 +15,16 @@ import java.util.List;
 public class RoomPostQueryServiceImpl implements RoomPostQueryService {
 
     private final RoomPostQueryRepository roomPostQueryRepository;
+    private final RoomTroublePostRepository roomTroublePostRepository;
 
     @Override
     public List<RoomPostResponseDto.PostListDto> getTop3RecentPosts(Long roomId) {
         return roomPostQueryRepository.findTop3RecentPostsWithUserInfo(roomId);
+    }
+
+    public List<RoomPostResponseDto.TroublePostSummaryDto> getPaginatedTroublePosts(Long boardId, int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        return roomTroublePostRepository.findTroublePostSummaries(boardId, (Pageable) pageRequest);
     }
 
 }
