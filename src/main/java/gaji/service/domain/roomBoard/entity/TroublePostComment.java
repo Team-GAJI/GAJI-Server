bd8a722f0b0a1d1dfccb50a7a2afd6847fec61d2
mmingoo <aldrn0323@naver.com>
2024-08-15 10:23:50 +0900

    ♻️ 댓글의 답글을 위한 TroublePostComment 수정

--- src/main/java/gaji/service/domain/roomBoard/entity/TroublePostComment.java
+++ src/main/java/gaji/service/domain/roomBoard/entity/TroublePostComment.java
@@ -5,13 +5,15 @@ import gaji.service.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @AllArgsConstructor
 public class TroublePostComment {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -26,11 +28,36 @@ public class TroublePostComment {
 
     private String body;
 
-    public void updateComment(String body){
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_comment_id")
+    private TroublePostComment parentComment;
+
+    @OneToMany(mappedBy = "parentComment", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<TroublePostComment> replies = new ArrayList<>();
+
+    private boolean isReply;
+
+    public void updateComment(String body) {
         this.body = body;
     }
 
     public boolean isAuthor(Long userId) {
         return this.user.getId().equals(userId);
+    }
+
+    public void addReply(TroublePostComment reply) {
+        if (!this.isReply) {
+            this.replies.add(reply);
+            reply.setParentComment(this);
+            reply.setIsReply(true);
+        }
+    }
+
+    public void setParentComment(TroublePostComment parentComment) {
+        this.parentComment = parentComment;
+    }
+
+    public void setIsReply(boolean isReply) {
+        this.isReply = isReply;
     }
 }
