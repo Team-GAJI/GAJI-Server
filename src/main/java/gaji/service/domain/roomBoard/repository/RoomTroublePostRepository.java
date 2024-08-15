9400f317a407d3a19414ef6fa4786b0366c56d1e
mmingoo <aldrn0323@naver.com>
2024-08-15 11:01:53 +0900

    ✨  게시글 조회를 위한 TroublePostSummaryDto 생성

--- src/main/java/gaji/service/domain/roomBoard/repository/RoomTroublePostRepository.java
+++ src/main/java/gaji/service/domain/roomBoard/repository/RoomTroublePostRepository.java
@@ -1,12 +1,14 @@
 package gaji.service.domain.roomBoard.repository;
 
 import gaji.service.domain.roomBoard.entity.RoomTroublePost;
+import gaji.service.domain.roomBoard.web.dto.RoomPostResponseDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.awt.print.Pageable;
+import java.util.List;
 
 @Repository
 public interface RoomTroublePostRepository extends JpaRepository<RoomTroublePost, Long> {
@@ -15,6 +17,6 @@ public interface RoomTroublePostRepository extends JpaRepository<RoomTroublePost
             "FROM RoomTroublePost r " +
             "WHERE r.roomBoard.id = :boardId " +
             "ORDER BY r.createdAt DESC")
-    List<TroublePostSummaryDto> findTroublePostSummaries(@Param("boardId") Long boardId, Pageable pageable);
+    List<RoomPostResponseDto.TroublePostSummaryDto> findTroublePostSummaries(@Param("boardId") Long boardId, Pageable pageable);
 
 }
