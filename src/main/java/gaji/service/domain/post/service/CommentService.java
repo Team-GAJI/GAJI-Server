04085f75cafab628ef16691dca8019c3db96bfa9
strongmhk <kim6562166086@gmail.com>
2024-08-16 11:41:47 +0900

    :sparkles: [#97] feature: 좋아요, 북마크, 게시글 삭제 검증

--- src/main/java/gaji/service/domain/post/service/CommentService.java
+++ src/main/java/gaji/service/domain/post/service/CommentService.java
@@ -1,14 +1,19 @@
 package gaji.service.domain.post.service;
 
 import gaji.service.domain.post.entity.Comment;
+import gaji.service.domain.post.entity.Post;
+import gaji.service.domain.post.web.dto.PostRequestDTO;
+import gaji.service.domain.user.entity.User;
 import org.springframework.data.domain.Slice;
 
 
 public interface CommentService {
 
     Comment saveNewComment(Comment comment);
+    Comment createCommentByCheckParentCommentIdIsNull(Long parentCommentId, PostRequestDTO.WriteCommentDTO request, User findUser, Post findPost);
     void hardDeleteComment(Comment comment);
     Comment findByCommentId(Long commentId);
     Slice<Comment> getCommentListByPost(Long postId, Integer lastGroupNum, int page, int size);
     void validCommentOwner(Long userId, Comment comment);
+
 }
