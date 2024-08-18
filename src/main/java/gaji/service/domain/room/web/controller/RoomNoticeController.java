065c89d7c3cea8f89387d5767b406e9e1f588c2f
shimfff <shimseawon1510@gmail.com>
2024-08-18 16:00:30 +0900

    :sparkles: Feat:swagger 설명 수정

--- src/main/java/gaji/service/domain/room/web/controller/RoomNoticeController.java
+++ src/main/java/gaji/service/domain/room/web/controller/RoomNoticeController.java
@@ -27,7 +27,7 @@ public class RoomNoticeController {
 
 
     @PostMapping("/notices/{roomId}")
-    @Operation(summary = "스터디룸 공지 등록 API",description = "스터디룸의 공지를 등록하는 API입니다. room의 id가 존재하는지, 작성자가 Reader인지 검증합니다.")
+    @Operation(summary = "스터디룸 공지 등록 API",description = "스터디룸의 공지를 등록하는 API입니다. room의 id가 존재하는지 검증합니다.")
     public BaseResponse<RoomResponseDto.RoomNoticeDto> NoticeController(
             @RequestBody @Valid RoomRequestDto.RoomNoticeDto requestDto,
             @RequestHeader("Authorization") String authorizationHeader,
