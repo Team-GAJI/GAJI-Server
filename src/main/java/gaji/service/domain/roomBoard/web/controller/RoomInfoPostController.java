17b86b89c6f70392140eaa14af2a5bbd9da88557
mmingoo <aldrn0323@naver.com>
2024-08-18 21:43:13 +0900

    ♻️ 각각의 컨트롤러에서 api에 대한 설명 추가

--- src/main/java/gaji/service/domain/roomBoard/web/controller/RoomInfoPostController.java
+++ src/main/java/gaji/service/domain/roomBoard/web/controller/RoomInfoPostController.java
@@ -173,7 +173,7 @@ public class RoomInfoPostController {
     }
 
     @GetMapping("/info/detail/{postId}")
-    @Operation(summary = "스터디룸 정보나눔 게시글 상세 조회")
+    @Operation(summary = "정보나눔 게시글 상세 조회", description = "1페이지를 불러오고 싶다면 page : 0으로 입력해야 합니다. ex) page 0 > 1페이지 / page 1 > 2페이지 ")
     public BaseResponse<RoomPostResponseDto.RoomInfoPostDetailDTO> getPostDetail(
             @RequestHeader("Authorization") String authorization,
             @PathVariable Long postId,
@@ -185,7 +185,7 @@ public class RoomInfoPostController {
     }
 
     @GetMapping("/info/{postId}/get/comments")
-    @Operation(summary = "스터디룸 정보나눔 게시글 댓글 및 답글 추가 로딩")
+    @Operation(summary = "정보나눔 게시글 댓글 및 답글 추가 로딩", description = "추가로 댓글을 무한 스크롤 형태로 불러올 때 호출하는 api 입니다")
     public BaseResponse<Page<RoomPostResponseDto.CommentWithRepliesDTO>> getMoreComments(
             @PathVariable Long postId,
             @RequestParam(defaultValue = "0") int page,
