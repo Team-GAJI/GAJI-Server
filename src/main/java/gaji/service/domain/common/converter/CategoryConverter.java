4be978960825c32e3441e8ff79999fbeb0b7f9d9
strongmhk <kim6562166086@gmail.com>
2024-08-21 12:57:30 +0900

    ✨ [#97] feature: 컨버터에서 예외 처리를 하도록 구현

--- src/main/java/gaji/service/domain/common/converter/CategoryConverter.java
+++ src/main/java/gaji/service/domain/common/converter/CategoryConverter.java
@@ -5,6 +5,8 @@ import gaji.service.domain.common.entity.SelectCategory;
 import gaji.service.domain.common.web.dto.CategoryResponseDTO;
 import gaji.service.domain.enums.CategoryEnum;
 import gaji.service.domain.enums.PostTypeEnum;
+import gaji.service.global.exception.RestApiException;
+import gaji.service.global.exception.code.status.GlobalErrorStatus;
 import org.springframework.core.convert.converter.Converter;
 
 import java.util.List;
@@ -15,7 +17,11 @@ public class CategoryConverter implements Converter<String, CategoryEnum> {
     // @RequestParam으로 String->CategoryEnum으로 convert할 때 필요한 메서드
     @Override
     public CategoryEnum convert(String source) {
-        return CategoryEnum.from(source);
+        CategoryEnum category = CategoryEnum.from(source);
+        if (category == null) {
+            throw new RestApiException(GlobalErrorStatus._INVALID_CATEGORY);
+        }
+        return category;
     }
 
     public static Category toCategory(CategoryEnum category) {
