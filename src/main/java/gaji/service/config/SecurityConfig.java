9e845ac2c9d8041868f28dd6a5837e5fcaf35fdf
strongmhk <kim6562166086@gmail.com>
2024-08-22 13:28:44 +0900

    ✨ [#97] feature: health-check api 구현

--- src/main/java/gaji/service/config/SecurityConfig.java
+++ src/main/java/gaji/service/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
     private static final String[] AUTH_WHITELIST = {
             "/oauth2/**", "/swagger-ui/**", "/api-docs", "/swagger-ui-custom.html",
             "/v3/api-docs/**", "/api-docs/**", "/swagger-ui.html", "/reissue", "/", "/my",
-            "/api/**", "/api/vote/**"
+            "/api/**", "/api/vote/**", "/health-check"
     };
 
     @Bean
