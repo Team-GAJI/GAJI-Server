51b637ff81c0c58fd62b8e4b9c4233f4a7da755b
karryred <ujin037@gmail.com>
2024-09-09 00:18:53 +0900

    :memo: Comment: 주석 추가

--- src/main/java/gaji/service/domain/user/service/UserCommandServiceImpl.java
+++ src/main/java/gaji/service/domain/user/service/UserCommandServiceImpl.java
@@ -20,6 +20,7 @@ public class UserCommandServiceImpl implements UserCommandService{
     private final UserRepository userRepository;
     private final UserQueryService userQueryService;
 
+    // TODO: 사용자를 상태를 업데이트
     @Override
     @Transactional
     public User cancleUser(Long userId) {
@@ -35,17 +36,20 @@ public class UserCommandServiceImpl implements UserCommandService{
         return user;
     }
 
+    // TODO: 사용자의 닉네임을 업데이트
     @Override
     @Transactional
     public User updateUserNickname(Long userIdFromToken, Long userIdFromPathVariable, UserRequestDTO.UpdateNicknameDTO request) {
         User user = userQueryService.findUserById(userIdFromToken);
 
+        // 닉네임을 변경하는 사용자가 본인인지 검증
         if(!user.equals(userQueryService.findUserById(userIdFromPathVariable))){
             throw new RestApiException(UserErrorStatus._USER_IS_NOT_SAME_);
         }
 
         String newNickname = request.getNickname();
 
+        // 기존에 사용하던 닉네임과 같은 지 검증
         if (user.getNickname()!=null && user.getNickname().equals(newNickname)) {
             throw new RestApiException(UserErrorStatus._NICKNAME_IS_SAME_);
         }
@@ -55,11 +59,14 @@ public class UserCommandServiceImpl implements UserCommandService{
         return user;
     }
 
+    // TODO: 비활성화 상태인 사용자를 DB에서 삭제
     @Override
     @Transactional
     public void hardDeleteInactiveUsers() {
+        // 비활성화 사용자 삭제 주기 설정
         LocalDateTime cutoffDate = LocalDateTime.now()
                 .minusDays(UserDeletePeriod.ONE_MONTH.getDays());
+
         userRepository.deleteAllByInactiveTimeBefore(cutoffDate);
     }
 
