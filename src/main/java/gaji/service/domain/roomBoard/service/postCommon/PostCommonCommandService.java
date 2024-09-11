f16d3651050efc318b9f2a6f95178c49c4430e91
mmingoo <aldrn0323@naver.com>
2024-09-11 17:37:04 +0900

    ♻️ RoomPost 댓글 삭제 기능 리팩토링
    주석을 추가, postCommonCommandService로 deleteReply, deleteCommentAndReplies 이동

--- src/main/java/gaji/service/domain/roomBoard/service/postCommon/PostCommonCommandService.java
+++ src/main/java/gaji/service/domain/roomBoard/service/postCommon/PostCommonCommandService.java
@@ -4,4 +4,8 @@ import gaji.service.domain.roomBoard.entity.RoomPost.PostComment;
 
 public interface PostCommonCommandService {
     void savePostComment(PostComment postComment);
+
+    void deleteReply(PostComment reply);
+
+    void deleteCommentAndReplies(PostComment comment);
 }
--- src/main/java/gaji/service/domain/roomBoard/service/postCommon/PostCommonCommandServiceImpl.java
+++ src/main/java/gaji/service/domain/roomBoard/service/postCommon/PostCommonCommandServiceImpl.java
@@ -12,8 +12,25 @@ import org.springframework.transaction.annotation.Transactional;
 public class PostCommonCommandServiceImpl implements PostCommonCommandService{
     private final PostCommentRepository postCommentRepository;
 
+    // TODO: 게시글 댓글 저장
     @Override
     public void savePostComment(PostComment postComment){
         postCommentRepository.save(postComment);
+    }
+
+    // TODO: 게시글 답글 삭제
+    @Override
+    public void deleteReply(PostComment reply) {
+        PostComment parentComment = reply.getParentComment();
+        parentComment.getReplies().remove(reply);
+        postCommentRepository.delete(reply);
+    }
+
+    // TODO: 댓글과 답글 삭제
+    @Override
+    public void deleteCommentAndReplies(PostComment comment) {
+        // CascadeType.ALL과 orphanRemoval = true 설정으로 인해
+        // 댓글을 삭제하면 연관된 모든 답글도 자동으로 삭제됩니다.
+        postCommentRepository.delete(comment);
     }
 }
